2e05584ba25ace95ebc74805985f1ec0cde08de8
James S <i@nixeagle.org>
2011-12-11 18:00:34 -0500

    Report only for 188 not 189

--- scripts.js
+++ scripts.js
@@ -3999,7 +3999,7 @@ beforeLogIn : function(src) {
             return;
         }
     }
-    var arr =  ["188.","189."];
+    var arr =  ["188."];
     for (var i = 0; i < arr.length; i++) {
         if (ip.substr(0, arr[i].length) == arr[i]) {
             sys.sendAll("Potential ban evader: " + sys.name(src) + " on IP: " + ip, staffchannel);
