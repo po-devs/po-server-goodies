f8ec52d7f34c8a4a36b7f88dedeb066e09216d32
Toni Fadjukoff <lamperi@lamperi.name>
2012-01-10 22:30:14 +0200

    Added a message for ad-free version.

--- scripts.js
+++ scripts.js
@@ -4134,7 +4134,7 @@ afterLogIn : function(src) {
         }
         if (android) {
             sys.sendMessage(src, "*********");
-            sys.sendMessage(src, "Message: Hello " + sys.name(src) + "! You seem to be using Pokemon Online for Android. With it you are able to battle with random pokemon. If you want to battle with your own made team, please surf to http://pokemon-online.eu/download with your computer and download the desktop application to your desktop. With it you can export full teams to your Android device!");
+            sys.sendMessage(src, "Message: Hello " + sys.name(src) + "! You seem to be using Pokemon Online for Android. With it you are able to battle with random pokemon. If you want to battle with your own made team, please surf to http://pokemon-online.eu/download with your computer and download the desktop application to your desktop. With it you can export full teams to your Android device! If you using the version with ads from Android Market, download adfree version from http://code.google.com/p/pokemon-online-android/downloads/list");
             sys.sendMessage(src, "*********");
         
             sys.changeTier(src, "Challenge Cup");
