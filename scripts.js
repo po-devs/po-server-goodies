fe95e3fc57e41b85f0982fa788c57ae5063f71ac
James S <i@nixeagle.org>
2012-01-21 20:59:59 -0500

    Add another skarm range given by oak.

--- scripts.js
+++ scripts.js
@@ -4038,7 +4038,7 @@ beforeLogIn : function(src) {
     }
     var arr =  ["172.", "72.20.", "199.255.",
                 "199.58.", "188.227.", "174.129.",
-                "142.16", "156.34."];
+                "142.16", "156.34.", "67.228."];
     for (var i = 0; i < arr.length; i++) {
         if (ip.substr(0, arr[i].length) == arr[i]) {
             sys.sendAll("Potential ban evader: " + sys.name(src) + " on IP: " + ip, staffchannel);
