c374af470b8b5733ad0296eb1194230809b8ee0b
James S <i@nixeagle.org>
2012-01-21 20:25:10 -0500

    I goofed, sys.ratedBattles not just ratedBattles

--- scripts.js
+++ scripts.js
@@ -301,7 +301,7 @@ var noPlayer = '*';
 var mafia = new function() {
     // Remember to update this if you are updating mafia
     // Otherwise mafia game won't get reloaded
-    this.version = "2012-01-22.1";
+    this.version = "2012-01-22.2";
 
     var CurrentGame;
     var PreviousGames;
@@ -1964,7 +1964,7 @@ mafiabot.sendAll("Importing old themes", mafiachan)
                     sys.sendMessage(src, "±Game: This IP is already in list. You cannot register two times!", mafiachan);
                     return;
                 }
-                if ((sys.ranking(src) <= 1000 && ratedBattles(src) < 5) ||
+                if ((sys.ranking(src) <= 1000 && sys.ratedBattles(src) < 5) ||
                     SESSION.users(src).smute.active) {
                     sys.sendMessage(src, "±Game: You need to ladder before playing mafia!", mafiachan);
                     return;
