2bb09a8ba1307b0a2556880999ce880335d20928
coyotte508 <coyotte508@hotmail.com>
2012-02-19 04:56:18 +0100

    Revert "Added watch command"
    This reverts commit cc8fbc643c421e83e52658c787cc6b6c09703db9.

--- scripts.js
+++ scripts.js
@@ -294,7 +294,6 @@ function POChannel(id)
     this.meoff = undefined;
     this.muted = {ips: {}};
     this.banned = {ips: {}};
-    this.watchers = [];
     this.ignorecaps = false;
     this.ignoreflood = false;
 }
@@ -654,7 +653,7 @@ function getplugins() {
     return SESSION.global().getplugins.apply(SESSION.global(), arguments);
 }
 
-SESSION.identifyScriptAs("PO Scripts v0.005");
+SESSION.identifyScriptAs("PO Scripts v0.004");
 SESSION.registerGlobalFactory(POGlobal);
 SESSION.registerUserFactory(POUser);
 SESSION.registerChannelFactory(POChannel);
@@ -794,8 +793,7 @@ var commands = {
         "/nameban regexp: Adds a regexp ban on usernames.",
         "/nameunban full_regexp: Removes a regexp ban on usernames.",
         "/destroychan [channel]: Destroy a channel (official channels are protected).",
-        "/channelusers [channel]: Lists users on a channel.",
-        "/watch [channel]: See the chat of a channel"
+        "/channelusers [channel]: Lists users on a channel."
     ],
     owner:
     [
@@ -2929,14 +2927,6 @@ adminCommand: function(src, command, commandData, tar) {
         }
         return;
     }
-    if (command == "watch") {
-        var cid = sys.channelId(commandData);
-        if (cid != undefined) {
-            SESSION.channels(cid).watchers.push(src);
-            channelbot.sendChanMessage(src, "You're now watching " + sys.channel(cid) + "!");
-            return;
-        }
-    }
      if (command == "megauseroff") {
         if (tar != undefined) {
             SESSION.users(tar).megauser = false;
@@ -4248,7 +4238,7 @@ afterChatMessage : function(src, message, chan)
             return;
         }
     }
-    SESSION.channels(channel).beforeMessage(src, message);
+
 } /* end of afterChatMessage */
 
 ,
