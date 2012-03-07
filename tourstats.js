a88d980eae9a09cd15eb8ca6fd35f75b12623939
Toni Fadjukoff <lamperi@lamperi.name>
2012-03-07 23:16:14 +0200

    Fix tourstats.

--- tourstats.js
+++ tourstats.js
@@ -5,7 +5,7 @@
 
 var tourwinners, tourstats, tourrankingsbytier;
 
-function updateTourStats(tier, time, winner, num, purgeTime, noPoints) {
+function updateTourStats(tier, time, winner, num, noPoints, purgeTime) {
     loadStats();
     var numToPoints = function() {
         if (noPoints) return 0;
@@ -67,6 +67,7 @@ function updateTourStats(tier, time, winner, num, purgeTime, noPoints) {
     var player;
     while (tourwinners.length > 0 && (parseInt(tourwinners[0][1]) + purgeTime) < time) {
         sys.sendMessage(sys.id("Lamperi"), "Hey lamperi we are purging tour winners: " + tourwinners[0], 0)
+        sys.sendMessage(sys.id("Lamperi"), "" + parseInt(tourwinners[0][1]) + " + " + purgeTime + " < " + time, 0)
         tier = tourwinners[0][0];
         points = numToPoints(tourwinners[0][2]);
         player = tourwinners[0][3];
