2de070943e57d4cc8584abd36da236cb48b2c71c
Crystal <mog-celes@hotmail.com>
2012-03-25 23:20:45 +0200

    Added html messaging which won't flash the user when sent. Hopefully this is temp until I can find a better way of doing it... sendHtmlAll is just there because I didn't really see the harm in adding it :x

--- bot.js
+++ bot.js
@@ -5,6 +5,10 @@ Bot.prototype.formatMsg = function(message)
 {
     return "±" + this.name + ": " + message;
 }
+Bot.prototype.formatHtmlMsg = function(message)
+{
+    return "<font color='#3DAA68'><timestamp/><b> ±" + this.name + "</b>:</font> " + message;
+}
 /* Shortcuts to sys functions */
 Bot.prototype.sendAll = function(message, channel)
 {
@@ -13,12 +17,26 @@ Bot.prototype.sendAll = function(message, channel)
     else
         sys.sendAll(this.formatMsg(message), channel);
 }
+Bot.prototype.sendHtmlAll = function(message, channel)
+{
+    if (channel === undefined)
+        sys.sendHtmlAll(this.formatHtmlMsg(message));
+    else
+        sys.sendHtmlAll(this.formatHtmlMsg(message), channel);
+}
 Bot.prototype.sendMessage = function(tar, message, channel)
 {
     if (channel === undefined)
         sys.sendMessage(tar, this.formatMsg(message));
     else
         sys.sendMessage(tar, this.formatMsg(message), channel);
+}
+Bot.prototype.sendHtmlMessage = function(tar, message, channel)
+{
+    if (channel === undefined)
+        sys.sendHtmlMessage(tar, this.formatHtmlMsg(message));
+    else
+        sys.sendHtmlMessage(tar, this.formatHtmlMsg(message), channel);
 }
 Bot.prototype.sendMainTour = function(message)
 {
