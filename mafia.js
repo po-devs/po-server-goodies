51aeb2051c6614f08f081cf19e4d30a41ec188ba
Toni Fadjukoff <lamperi@lamperi.name>
2012-04-11 07:28:10 +0300

    Added poison.mode.evadeChance (idea by Hoboman)
    - Small fix: evadeChance for poison and kill
      have now correct test (it was off by one)

--- mafia.js
+++ mafia.js
@@ -1160,6 +1160,8 @@ function Mafia(mafiachan) {
                                 mafia.sendPlayer(player.name, "±Game: Your target (" + target.name + ") was guarded!");
                             } else if ("poison" in target.role.actions && target.role.actions.poison.mode == "ignore") {
                                   mafia.sendPlayer(player.name, "±Game: Your target (" + target.name + ") was immune to the poison!");
+                            } else if ("poison" in target.role.actions && typeof target.role.actions.poison.mode == "object" && target.role.actions.poison.mode.evadeChance < sys.rand(0,100)/100) {
+                                mafia.sendPlayer(player.name, "±Game: Your target (" + target.name + ") evaded the poison! Somehow.");
                             } else if (target.poisoned === undefined) {
                                 mafia.sendPlayer(player.name, "±Game: Your target (" + target.name + ") was poisoned!");
                                 target.poisoned = 1;
@@ -1196,7 +1198,7 @@ function Mafia(mafiachan) {
                                 mafia.sendPlayer(player.name, "±Game: Your target (" + target.name + ") was protected!");
                             } else if ("kill" in target.role.actions && target.role.actions.kill.mode == "ignore") {
                                 mafia.sendPlayer(player.name, "±Game: Your target (" + target.name + ") evaded the kill!");
-                            } else if ("kill" in target.role.actions && typeof target.role.actions.kill.mode == "object" && target.role.actions.kill.mode.evadeChance < sys.rand(1,100)/100) {
+                            } else if ("kill" in target.role.actions && typeof target.role.actions.kill.mode == "object" && target.role.actions.kill.mode.evadeChance < sys.rand(0,100)/100) {
                                 mafia.sendPlayer(player.name, "±Game: Your target (" + target.name + ") evaded the kill!");
                             } else {
                                 if (mafia.theme.killusermsg) {
