01d5af56432c03b6a432dc91d0f4c083c3646f8f
Elias <viderizer@gmail.com>
2012-06-06 18:53:39 +0300

    Revert "Make tour start nonFlashing"
    This reverts commit 6aced599262a6e4f4102c697853d0317cc84da0f.

--- tournaments.js
+++ tournaments.js
@@ -31,8 +31,6 @@ var clauseMap = {
 	128: WIFI_CLAUSE,
 	256: SELF_KO_CLAUSE
 };
-var utilities = require('utilities.js');
-var nonFlashing = utilities.non_flashing;
 
 // build reverse mapping
 var clauseToId={};
@@ -214,7 +212,7 @@ function Tournament(channel)
 		self.mode = mode;
 
 		wall(border);
-		wall("*** A Tournament was started by " + nonFlashing(self.starter) + "! ***");
+		wall("*** A Tournament was started by " + self.starter + "! ***");
 		wall("PLAYERS: " + self.count);
 		wall("TIER: " + self.tier);
 		wall("MODE: " + self.mode);
