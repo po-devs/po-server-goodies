726b8b8ef17a9fd35d13a63b0e8f9f631548a8fe
coyotte508 <coyotte508@hotmail.com>
2012-07-06 06:35:31 +0200

    tournaments.js updated to v2

--- tournaments.js
+++ tournaments.js
@@ -2,7 +2,7 @@
  * tournaments.js
  *
  * Contains code for pokemon online server scripted tournaments.
- * Original code by unknown.
+ * Original code by coyotte508/Lutra.
  */
 if (typeof Config == "undefined")
 	Config = {};
@@ -360,8 +360,7 @@ function Tournament(channel)
 			return;
 		}
 
-		var srctier = sys.tier(source);
-		if (!cmp(srctier, self.tier)){
+        if (!sys.hasTier(source, self.tier)){
 			sendPM(source, "You are currently not battling in the " + self.tier + " tier. Change your tier to " + self.tier + " to be able to join.");
 			return;
 		}
@@ -410,8 +409,7 @@ function Tournament(channel)
 			return;
 		}
 
-		var srctier = sys.tier(source);
-		if (!cmp(srctier, self.tier)){
+        if (!sys.hasTier(source, self.tier)){
 			sendPM(source, "You are currently not battling in the " + self.tier + " tier. Change your tier to " + self.tier + " to be able to join.");
 			return;
 		}
@@ -957,7 +955,7 @@ function Tournament(channel)
 	}
 
 	// event beforeChallenge
-	function beforeChallenge(source, dest, clauses, rated, mode) {
+    function beforeChallenge(source, dest, clauses, rated, mode, team, destTier) {
 		if (!playingPhase())
 			return;
 		var name1 = sys.name(source),
@@ -967,7 +965,8 @@ function Tournament(channel)
 				sendPM(source, "This guy isn't your opponent in the tourney.");
 				return true;
 			}
-            if (sys.tier(source) != sys.tier(dest) || !cmp(sys.tier(source), self.tier)) {
+            var srcTier = sys.tier(source, team);
+            if (srcTier != destTier || !cmp(srcTier, self.tier)) {
 				sendPM(source, "You must be both in the tier " + self.tier + " to battle in the tourney.");
 				return true;
 			}
@@ -1191,10 +1190,10 @@ module.exports = {
 		}
 	},
 
-	beforeChallengeIssued : function(source, dest, clauses, rated, mode) {
+    beforeChallengeIssued : function(source, dest, clauses, rated, mode, team, destTier) {
 		var ret = false;
 		for (var channel in module.tournaments) {
-			ret |= module.tournaments[channel].events.beforeChallengeIssued(source, dest, clauses, rated, mode);
+            ret |= module.tournaments[channel].events.beforeChallengeIssued(source, dest, clauses, rated, mode, team, destTier);
 		}
 		return ret;
 	},
