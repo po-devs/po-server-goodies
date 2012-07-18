7c64f30890f8a77697d84773d1e7ee87f20c821c
Crystal <mog-celes@hotmail.com>
2012-07-18 22:04:47 +0200

    Looking for myself And if it works fine, I'm gonna kill people, because I'm too tired for this.

--- trivia.js
+++ trivia.js
@@ -152,9 +152,10 @@ try { // Do not indent this, it is only until this starts to work
                 var responseTime = this.submittedAnswers[id].time;
                 var realTime = time();
                 var minus = realTime - responseTime;
-                var pointAdd = minus > 6 ? 5 : (minus < 7 && minus > 3 ? 3 : 2);
-                //sys.sendMessage(sys.id("Lamperi"), "TriviaPointDebug: took" + minus + " seconds, point add is " + pointAdd + ".", triviachan);
-				// TODO: check answer length, and base pointAdd off of that?
+                //var pointAdd = minus > 6 ? 5 : (minus < 7 && minus > 3 ? 3 : 2);
+                var pointAdd = minus < 7 ? 5 : (minus < 9 ? 3 : 1);
+                sys.sendMessage(sys.id("Crystal Moogle"), "TriviaPointDebug: took" + minus + " seconds, point add is " + pointAdd + ".", triviachan);
+                // TODO: check answer length, and base pointAdd off of that?
 
                 answeredCorrectly.push(name);
                 this.player(name).points += pointAdd;
