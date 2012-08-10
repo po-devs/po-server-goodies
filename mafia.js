dc63d15dd630476235e3782a7cb15cc4987b61da
nixeagle <i@nixeagle.org>
2012-08-10 20:32:23 -0300

    fix?

--- mafia.js
+++ mafia.js
@@ -15,7 +15,7 @@ var is_command = require("utilities.js").is_command;
 function Mafia(mafiachan) {
     // Remember to update this if you are updating mafia
     // Otherwise mafia game won't get reloaded
-    this.version = "2012-01-10.2";
+    this.version = "2012-08-10.3";
     var mafia = this;
 
     var noPlayer = '*';
