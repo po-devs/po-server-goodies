0550a3d31549a7d9d0828f131889a20937d32935
Jirachier <rida-fathi@hotmail.com>
2012-08-30 08:54:16 +0000

    minor

--- amoebagame.js
+++ amoebagame.js
@@ -24,13 +24,13 @@ module.exports = function() {
         var role = SESSION.users(src).amoeba.role;
         if (sys.auth(src) > 0 && ["/","!"].indexOf(message[0]) > -1) return false;
         if (role != species.length - 1) {
-            sys.sendAll(sys.name(src) + ": " + messages[role], id);
+            sendChanAll(sys.name(src) + ": " + messages[role], id);
             if (battles[role] && battles[role] != src && sys.isInChannel(battles[role], id)) {
                 var winner = sys.rand(0,2) > 0 ? battles[role] : src;
                 var loser = winner == src ? battles[role] : src;
                 SESSION.users(winner).amoeba.role++;
                 if (SESSION.users(loser).amoeba.role > 0) SESSION.users(loser).amoeba.role--;
-                sys.sendAll("+EvolutionBot: Two " + species[role] + "s, "
+                sendChanAll("+EvolutionBot: Two " + species[role] + "s, "
                     + sys.name(battles[role]) + " and " + sys.name(src) + " engaged in a fierce battle! "
                     + sys.name(winner) + " won and is now a " + species[SESSION.users(winner).amoeba.role] + ". "
                     + sys.name(loser) + " lost and is now a " + species[SESSION.users(loser).amoeba.role] + ".",
