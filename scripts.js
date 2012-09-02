9391000753f5c3e8ecb274991aff19021f863b00
Jirachier <rida-fathi@hotmail.com>
2012-09-02 20:38:01 +0000

    minor

--- scripts.js
+++ scripts.js
@@ -190,7 +190,7 @@ getVal = function(valname) { // Removes ":" if it's the first character of the v
 };
 
 escape_dq = function(txt) { // doublequotes escaping and backslashes
-    return txt.replace(/"/g, '\"').replace(/\\/g, '\\');
+    return txt.replace(/"/g, '\\"').replace(/\\/g, '\\');
 };
 
 append_logs = function(params) { // Adds chat lines to the logs
