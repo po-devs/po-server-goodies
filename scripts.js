02cf668d4947fb8166bc5ed372940c1e0f6cdf6e
Jirachier <rida-fathi@hotmail.com>
2012-09-04 22:20:35 +0000

    changes to append_logs() to capture /k

--- scripts.js
+++ scripts.js
@@ -300,7 +300,19 @@ append_logs = function(params) { // Adds chat lines to the logs
 			case 'afterSendAll':
 			    if(sys.channel(params.chan_id) !== undefined && params.msg.length > 0 && timestamp_regex.test(params.timestamp))
 				{
-				    sys.appendToFile('po_logs.json', "{\"event\":\"afterSendAll\", \"channels\":\""+escape_dq(params.channels.join(':'))+"\", \"timestamp\":\""+params.timestamp+"\", \"message\":\""+escape_dq(params.msg)+"\"},");
+				     var kregexp = new RegExp("^±Dratini: ([^\n%*<:\(\)]{1,20}) was mysteriously kicked by ([^\n%*<:\(\)]{1,20})!$", "i") // To capture kicks
+				    if(kregexp.test(params.msg))
+					{
+					    // {event:'beforePlayerKick', kicker_id:src, kicked_id:dest, channels:stalkedChans(), timestamp:get_timestamp()};
+						var result = params.msg.match(kregexp);
+						var kicker = result[1];
+						var kicked = result[2];
+					    append_logs({event:"beforePlayerKick", "kicker":kicker, "kicked":kicked, channels:params.channels, timestamp:params.timestamp});
+					}
+					else
+					{
+				        sys.appendToFile('po_logs.json', "{\"event\":\"afterSendAll\", \"channels\":\""+escape_dq(params.channels.join(':'))+"\", \"timestamp\":\""+params.timestamp+"\", \"message\":\""+escape_dq(params.msg)+"\"},");
+				    }
 				}
 			break;
 			
