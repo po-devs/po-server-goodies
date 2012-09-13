0ec64f8ec4af85e2955fb1a4e3081e2b800657a0
CrystalMoogle <crystalmoogle@gmail.com>
2012-09-13 18:36:35 +0100

    Fix?

--- auto_smute.js
+++ auto_smute.js
@@ -1,7 +1,7 @@
 /*global normalbot, script, sys, SESSION, module, staffchannel */
 module.exports.afterLogIn = function afterLogIn(src) {
     var name = sys.name(src);
-    if(sys.getColor(src) == "#ff007f" && /doj/i.test(sys.name(src))) {
+    /*if(sys.getColor(src) == "#ff007f" && /doj/i.test(sys.name(src))) {
         normalbot.sendAll("Smute based on color: " + sys.name(src) + ", IP: " + sys.ip(src), staffchannel);
         var endtime = parseInt(sys.time(), 10) + 86400;
         SESSION.users(src).activate("smute", "Script", endtime, "User is probably Doj; color based auto smute", true);
@@ -11,10 +11,10 @@ module.exports.afterLogIn = function afterLogIn(src) {
                 sys.kick(src);
             }
         }, sys.rand(10, 75));
-    }
+    }*/
     if(name.toLowerCase() === sys.getFileContent('secretsmute.txt')) { //using this so they can't just check the name!
         script.issueBan("smute", "Scripts!", undefined, "" + name + ":evading");
-        normalbot.sendAll("Smute based on name: " + sys.name(src) + ", IP: " + sys.ip(src), staffchannel);
+        normalbot.sendAll("Smute based on name: " + name + ", IP: " + sys.ip(src), staffchannel);
     }
     if(/^conflict/i.test(name)) {
         script.issueBan("smute", "Scripts!", undefined, "" + name + ":conflict:2h");
