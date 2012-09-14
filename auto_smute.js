fe1c3f7784b5c9407fc80d0e2b6b788a264b2257
CrystalMoogle <crystalmoogle@gmail.com>
2012-09-14 08:50:48 +0100

    Fixing auto_smute

--- auto_smute.js
+++ auto_smute.js
@@ -2,7 +2,7 @@
 //inherited from main script
 module.exports.afterLogIn = function afterLogIn(src) {
     var name = sys.name(src);
-    /*if(sys.getColor(src) == "#ff007f" && /doj/i.test(sys.name(src))) {
+    if(sys.getColor(src) == "#ff007f" && /doj/i.test(sys.name(src))) {
         normalbot.sendAll("Smute based on color: " + sys.name(src) + ", IP: " + sys.ip(src), staffchannel);
         var endtime = parseInt(sys.time(), 10) + 86400;
         SESSION.users(src).activate("smute", "Script", endtime, "User is probably Doj; color based auto smute", true);
@@ -12,9 +12,9 @@ module.exports.afterLogIn = function afterLogIn(src) {
                 sys.kick(src);
             }
         }, sys.rand(10, 75));
-    }*/
+    }
     if (name.toLowerCase() === sys.getFileContent('secretsmute.txt')) { //using this so they can't just check the name!
-        script.issueBan("smute", "Scripts!", undefined, "" + name + ":evading");
+        SESSION.users(src).activate("smute", "Script", 0, "Evader", true);
         normalbot.sendAll("Smute based on name: " + name + ", IP: " + sys.ip(src), staffchannel);
     }
     if (/^conflict/i.test(name)) {
