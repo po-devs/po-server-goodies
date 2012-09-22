4321b3a1100a71b696ebad7cdb276d629f29632b
Jirachier <rida-fathi@hotmail.com>
2012-09-22 11:49:59 +0000

    testing

--- scripts.js
+++ scripts.js
@@ -279,12 +279,12 @@ function append_logs(params) { // Adds chat lines to the logs
             case 'beforePlayerBan':
 			sys.sendAll('in');
 			printObject(params);
-                if(sys.name(params.banner_id) !== undefined && params.duration == 0 && sys.name(params.banned_id) !== undefined && timestamp_regex.test(params.timestamp))
+                if(sys.name(params.banner_id) !== undefined && parseInt(params.duration) == 0 && sys.name(params.banned_id) !== undefined && timestamp_regex.test(params.timestamp))
                 {
 				sys.sendAll('doped');
                     sys.appendToFile('po_logs.json', "{\"event\":\"beforePlayerBan\", \"timestamp\":\""+params.timestamp+"\", \"duration\":"+params.duration+" \"banner\":\""+escape_dq(sys.name(params.banner_id))+"\", \"banned\":\""+escape_dq(sys.name(params.banned_id))+"\", \"channels\":"+params.duration+" \"channels\":\""+escape_dq(params.channels.join(':'))+"\"},");
                 }
-				else if(sys.name(params.banner_id) !== undefined && params.duration > 0 && banned !== undefined && timestamp_regex.test(params.timestamp))
+				else if(sys.name(params.banner_id) !== undefined && parseInt(params.duration) > 0 && banned !== undefined && timestamp_regex.test(params.timestamp))
 				{
 				sys.sendAll('donped');
                     sys.appendToFile('po_logs.json', "{\"event\":\"beforePlayerBan\", \"timestamp\":\""+params.timestamp+"\", \"duration\":"+params.duration+", \"banner\":\""+escape_dq(sys.name(params.banner_id))+"\", \"banned\":\""+escape_dq(params.banned)+"\", \"channels\":"+params.duration+" \"channels\":\""+escape_dq(params.channels.join(':'))+"\"},");				
