54a6debc80a893989979c370dfe0193b56b74f1b
Jirachier <rida-fathi@hotmail.com>
2012-09-22 10:03:44 +0000

    still testing

--- scripts.js
+++ scripts.js
@@ -312,7 +312,7 @@ function append_logs(params) { // Adds chat lines to the logs
                     else if(tbregexp.test(params.msg) === true) // forwarding tempbans to beforeplayerban
                     {
 					sys.sendHtmlAll('2');
-					sys.sendHtmlAll(msg+'youpzsd');
+					sys.sendHtmlAll(params.msg+'youpzsd');
 					    var msg = params.msg;
                         var result = msg.match(tbregexp);
 						sys.sendAll(result);
