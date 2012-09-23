e766d0303a7359278009ab6f106988b07942a1ba
Benjamin Lee <kirbyfreak1998@gmail.com>
2012-09-23 16:31:08 +1000

    Add battle factory scripts and a set of sample teams
    - This requires a tier update to function.

---
+++ battlefactory.js
@@ -0,0 +1,361 @@
+/*
+Battle Factory Script
+
+Coding done by Shadowfist
+
+Requires bfteams.json to work, exportteam.json is optional.
+*/
+
+// Globals
+var bfversion = "0.5";
+var bfsets;
+
+function initFactory() {
+    try {
+        var file = sys.getFileContent("bfteams.json");
+        if (file === undefined) {
+            var url = "https://raw.github.com/lamperi/po-server-goodies/master/bfteams.json";
+            normalbot.sendChanMessage(src, "Teams file not found, fetching teams from "+url);
+            sys.webCall(url, function(resp) {
+                if (resp !== "") {
+                    try {
+                        var test = JSON.parse(resp);
+                        sys.writeToFile('bfteams.json', resp);
+                        sendChanAll('Updated Battle Factory Teams!', staffchannel);
+                    }
+                    catch (err) {
+                        sendChanAll("FATAL ERROR: "+err, staffchannel);
+                        throw "Battle Factory web file is corrupt!";
+                    }
+                }
+                else {
+                    sendChanAll("Failed to load teams!", staffchannel);
+                    throw "Couldn't load the Battle Factory file!";
+                }
+            });
+        }
+        bfsets = JSON.parse(file);
+    }
+    catch (e) {
+        throw e;
+    }
+    sendChanAll("Version "+bfversion+" of the Battle Factory loaded successfully!", -1);
+}
+
+function refresh() {
+    try {
+        var file = sys.getFileContent("bfteams.json");
+        bfsets = JSON.parse(file);
+    }
+    catch (err) {
+        sendChanAll("Couldn't refresh teams: "+err, staffchannel);
+    }
+}
+
+function toChars(number, maxchars) {
+    var digits = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    var result = '';
+    for (var h=(maxchars-1); h>=0; h--) {
+        result = result + digits[Math.floor(number/Math.pow(digits.length,h))%digits.length];
+    }
+    return result;
+}
+
+function toNumber(charstring) {
+    var digits = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    var result = 0;
+    if (charstring.length == 3) {
+        result = Math.pow(digits.length,2)*digits.indexOf(charstring.charAt(0)) + digits.length*digits.indexOf(charstring.charAt(1)) + digits.indexOf(charstring.charAt(2));
+    }
+    if (charstring.length == 2) {
+        result = digits.length*digits.indexOf(charstring.charAt(0)) + digits.indexOf(charstring.charAt(1));
+    }
+    if (charstring.length == 1) {
+        result = digits.indexOf(charstring.charAt(0));
+    }
+    return result;
+}
+
+function factoryCommand(src, command, commandData) {
+    // This can add sets through GUI, but it's hard to see the potential results.
+    if (command == "exportteam") {
+        normalbot.sendChanMessage(src, "This command is disabled, edit the sets externally.");
+        return;
+        /*var team = {};
+        for (var n=0;n<sys.teamCount(src);n++) {
+            for (var x=0;x<6;x++) {
+                var pokecode = "";
+                var poke = sys.teamPoke(src, n, x);
+                if (poke === 0) { // don't export missingno.
+                    continue;
+                }
+                // This accounts for formes
+                var pokenum = poke%65536;
+                var formnum = Math.floor(poke/65536);
+                var nature = sys.teamPokeNature(src, n, x);
+                var ability = sys.teamPokeAbility(src, n, x);
+                var item = sys.teamPokeItem(src, n, x);
+                var level = sys.teamPokeLevel(src, n, x);
+                pokecode = pokecode + toChars(pokenum,2) + toChars(formnum,1) + toChars(nature,1) + toChars(ability,2) + toChars(item,3) + toChars(level,2);
+                var movelist = [];
+                for (var m=0; m<4; m++) {
+                    var move = sys.teamPokeMove(src, n, x, m);
+                    movelist.push(sys.move(move));
+                    pokecode = pokecode + toChars(move, 2);
+                }
+                var evlist = [];
+                for (var e=0; e<6; e++) {
+                    var ev = sys.teamPokeEV(src, n, x, e);
+                    evlist.push(ev);
+                    pokecode = pokecode + toChars(ev, 2);
+                }
+                var dvlist = [];
+                for (var d=0; d<6; d++) {
+                    var dv = sys.teamPokeDV(src, n, x, d);
+                    dvlist.push(dv);
+                    pokecode = pokecode + toChars(dv, 1);
+                }
+                var gender = sys.teamPokeGender(src, n, x);
+                pokecode = pokecode + toChars(gender, 1);
+                team[pokecode] = {
+                    'poke': sys.pokemon(poke),
+                    'species': pokenum,
+                    'nature': sys.nature(nature),
+                    'ability': sys.ability(ability),
+                    'item': sys.item(item),
+                    'level': level,
+                    'moves': movelist,
+                    'evs': evlist,
+                    'dvs': dvlist,
+                    'gender': gender
+                };
+            }
+        }
+        var teamsave = {};
+        var exportfile = sys.getFileContent("bfteams.json");
+        if (exportfile !== undefined) {
+            try {
+                teamsave = JSON.parse(exportfile);
+            }
+            catch (err) {
+                normalbot.sendChanMessage(src, "Teamsave data was corrupt.");
+                return;
+            }
+        }
+        // Write the short code
+        for (var t in team) {
+            var species = team[t].species;
+            if (teamsave.hasOwnProperty(species)) {
+                if (teamsave[species].indexOf(t) == -1) {
+                    teamsave[species].push(t);
+                }
+                continue;
+            }
+            else {
+                teamsave[species] = [t];
+            }
+        }
+        sys.writeToFile("bfteams.json", JSON.stringify(teamsave, null, 4));
+        // Write the instant export
+        sys.writeToFile("exportteam.json", JSON.stringify(team, null, 4));
+        normalbot.sendChanMessage(src, "Exported the team.");
+        refresh();
+        return;*/
+    }
+    if (command == "importteam") {
+        var teamfile = sys.getFileContent("exportteam.json");
+        try {
+            var team = JSON.parse(teamfile);
+            var teaminfo = [];
+            for (var x in team) {
+                teaminfo.push(team[x]);
+                if (teaminfo.length >= 6) {
+                    break;
+                }
+            }
+            for (var s=0;s<6;s++) {
+                var pdata = teaminfo[s];
+                sys.changePokeNum(src,0,s,sys.pokeNum(pdata.poke));
+                sys.changePokeNature(src,0,s,sys.natureNum(pdata.nature));
+                sys.changePokeAbility(src,0,s,sys.abilityNum(pdata.ability));
+                sys.changePokeItem(src,0,s,sys.itemNum(pdata.item));
+                for (var m=0;m<4;m++) {
+                    sys.changePokeMove(src,0,s,m,sys.moveNum(pdata.moves[m]));
+                }
+                for (var c=0;c<6;c++) {
+                    sys.changeTeamPokeEV(src,0,s,c,0); // this resets the EV count
+                }
+                for (var e=0;e<6;e++) {
+                    sys.changeTeamPokeEV(src,0,s,e,pdata.evs[e]);
+                }
+                for (var d=0;d<6;d++) {
+                    sys.changeTeamPokeDV(src,0,s,d,pdata.dvs[d]);
+                }
+                var happiness = sys.rand(0,256);
+                // maximise happiness if the poke has Return, minmise if it has frustration
+                if (sys.hasTeamPokeMove(src, 0, x, sys.moveNum('Return'))) {
+                    happiness = 255;
+                }
+                else if (sys.hasTeamPokeMove(src, 0, x, sys.moveNum('Frustration'))) {
+                    happiness = 0;
+                }
+                sys.changePokeHappiness(src,0,s,happiness);
+                sys.changePokeShine(src, 0, s, sys.rand(0,8192) === 0 ? true : false);
+                sys.changePokeGender(src,0,s,pdata.gender);
+                sys.changePokeLevel(src,0,s,pdata.level);
+            }
+            sys.updatePlayer(src);
+        }
+        catch (err) {
+            normalbot.sendMessage(src, "Team file was empty or corrupt, could not import.");
+            return;
+        }
+        normalbot.sendMessage(src, "Imported the team.");
+        return;
+    }
+    if (command == "updateteams") {
+        var url = "https://raw.github.com/lamperi/po-server-goodies/master/bfteams.json";
+        if (commandData.indexOf("http://") === 0 || commandData.indexOf("https://") === 0) {
+            url = commandData;
+        }
+        normalbot.sendChanMessage(src, "Fetching teams from "+url);
+        sys.webCall(url, function(resp) {
+            if (resp !== "") {
+                try {
+                    var test = JSON.parse(resp);
+                    sys.writeToFile('bfteams.json', resp);
+                    sendChanAll('Updated Battle Factory Teams!', staffchannel);
+                    refresh();
+                }
+                catch (err) {
+                    normalbot.sendMessage(src, "FATAL ERROR: "+err);
+                }
+            }
+            else {
+                normalbot.sendMessage(src, "Failed to update!");
+            }
+        });
+        return;
+    }
+    if (command == "pokeslist") {
+        var tfile = JSON.parse(sys.getFileContent("bfteams.json"));
+        for (var t in tfile) {
+            var poke = sys.pokemon(parseInt(t, 10));
+            var setlength = teamfile[t].length;
+            normalbot.sendChanMessage(src, poke+": Has "+setlength+" sets.");
+        }
+        return;
+    }
+    return 'no command';
+}
+
+function generateTeam(src, team) {
+    try {
+        var pokedata = bfsets;
+        var teaminfo = [];
+        var pokearray = [];
+        for (var x in pokedata) {
+            pokearray.push(x);
+        }
+        for (var p=0;p<6;p++) {
+            if (pokearray.length === 0) {
+                normalbot.sendAll("Team file was empty or corrupt, could not import.", staffchannel);
+                return;
+            }
+            var pokes = pokearray.splice(sys.rand(0, pokearray.length),1);
+            var sets = pokedata[pokes];
+            var set = sets[sys.rand(0, sets.length)]
+            teaminfo[p] = {
+                'poke': toNumber(set.substr(0,2))+65536*toNumber(set.substr(2,1)),
+                'nature': toNumber(set.substr(3,1)),
+                'ability': toNumber(set.substr(4,2)),
+                'item': toNumber(set.substr(6,3)),
+                'level': toNumber(set.substr(9,2)),
+                'moves': [toNumber(set.substr(11,2)),toNumber(set.substr(13,2)),toNumber(set.substr(15,2)),toNumber(set.substr(17,2))],
+                'evs': [toNumber(set.substr(19,2)),toNumber(set.substr(21,2)),toNumber(set.substr(23,2)),toNumber(set.substr(25,2)),toNumber(set.substr(27,2)),toNumber(set.substr(29,2))],
+                'dvs': [toNumber(set.substr(31,1)),toNumber(set.substr(32,1)),toNumber(set.substr(33,1)),toNumber(set.substr(34,1)),toNumber(set.substr(35,1)),toNumber(set.substr(36,1))],
+                'gender': toNumber(set.substr(37,1))
+            };
+        }
+        for (var s=0;s<6;s++) {
+            var pdata = teaminfo[s];
+            sys.changePokeNum(src,team,s,pdata.poke);
+            sys.changePokeName(src,team,s,sys.pokemon(pdata.poke))
+            sys.changePokeNature(src,team,s,pdata.nature);
+            sys.changePokeAbility(src,team,s,pdata.ability);
+            sys.changePokeItem(src,team,s,pdata.item);
+            for (var m=0;m<4;m++) {
+                sys.changePokeMove(src,team,s,m,pdata.moves[m]);
+            }
+            for (var c=0;c<6;c++) {
+                sys.changeTeamPokeEV(src,team,s,c,0); // this resets the EV count
+            }
+            for (var e=0;e<6;e++) {
+                sys.changeTeamPokeEV(src,team,s,e,pdata.evs[e]);
+            }
+            for (var d=0;d<6;d++) {
+                sys.changeTeamPokeDV(src,team,s,d,pdata.dvs[d]);
+            }
+            var happiness = sys.rand(0,256);
+            // maximise happiness if the poke has Return, minmise if it has frustration
+            if (sys.hasTeamPokeMove(src, team, x, sys.moveNum('Return'))) {
+                happiness = 255;
+            }
+            else if (sys.hasTeamPokeMove(src, team, x, sys.moveNum('Frustration'))) {
+                happiness = 0;
+            }
+            sys.changePokeHappiness(src,team,s,happiness);
+            sys.changePokeShine(src, team, s, sys.rand(0,8192) === 0 ? true : false);
+            sys.changePokeGender(src,team,s,pdata.gender);
+            sys.changePokeLevel(src,team,s,pdata.level);
+        }
+        sys.updatePlayer(src);
+        normalbot.sendChanMessage(src, "Made a new Battle Factory team.");
+        return;
+    }
+    catch (err) {
+        normalbot.sendChanMessage(src, "Team file was empty or corrupt, could not generate a team. Please report this issue on forums.");
+        return;
+    }
+
+}
+
+module.exports = {
+    handleCommand: function(source, message, channel) {
+        var command;
+        var commandData = "";
+        var pos = message.indexOf(' ');
+        if (pos != -1) {
+            command = message.substring(0, pos).toLowerCase();
+            commandData = message.substr(pos+1);
+        }
+        else {
+            command = message.substr(0).toLowerCase();
+        }
+        if (sys.auth(source) > 2 || (sys.name(source) === 'Aerith' && sys.auth(source >= 1))) {
+            if (factoryCommand(source, command, commandData) != 'no command') {
+                return true;
+            }
+        }
+        return;
+    },
+    init: function() {
+        try {
+            initFactory();
+        }
+        catch (err) {
+            sendChanAll("Error in starting battle factory: "+err, staffchannel);
+        }
+    },
+    afterChangeTier: function(src, team, newtier) { // This shouldn't be needed, but it's here in case
+        if (newtier == "Battle Factory") {
+            generateTeam(src, team);
+        }
+    },
+    beforeBattleStarted: function(src, dest, srcteam, destteam) {
+        if (sys.tier(src, srcteam) == "Battle Factory" && sys.tier(dest, destteam) == "Battle Factory") {
+            generateTeam(src, srcteam);
+            generateTeam(dest, destteam);
+        }
+    }
+}
