bc5156a8667f878a26c82ef85eebd54ef531967e
CrystalMoogle <crystalmoogle@gmail.com>
2012-09-24 15:55:58 +0100

    I'll remember to remove this one day

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // This is the official Pokemon Online Scripts
 // These scripts will only work on 2.0.00 or newer.
-/*jshint "laxbreak":true,"shadow":true,"undef":true,"evil":true,"trailing":true,"proto":true,"withstmt":true*/
+
 // You may change these variables as long as you keep the same type
 var Config = {
     base_url: "https://raw.github.com/lamperi/po-server-goodies/master/",
