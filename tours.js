165e24e429daecebf11ee52b5cbc2c5a0abded89
Benjamin Lee <kirbyfreak1998@gmail.com>
2012-10-04 17:34:39 +1000

    permission changes
    Users have to be auth 1+ to add megausers

--- tours.js
+++ tours.js
@@ -1707,7 +1707,7 @@ function tourCommand(src, command, commandData) {
         }
         if (isTourSuperAdmin(src)) {
             /* Tournament Admins etc. */
-            if (command == "tadmin" || command == "tadmins" || command == "megauser" || (isTourOwner(src) && (command == "tsadmin" || command == "tsadmins")) || (sys.auth(src) >= 3 && (command == "towner" || command == "towners"))) {
+            if ((sys.auth(src) >= 1 && (command == "tadmin" || command == "tadmins" || command == "megauser"))  || (isTourOwner(src) && (command == "tsadmin" || command == "tsadmins")) || (sys.auth(src) >= 3 && (command == "towner" || command == "towners"))) {
                 var tadmins = tours.touradmins
                 if (sys.dbIp(commandData) === undefined) {
                     sendBotMessage(src,"This user doesn't exist!",tourschan,false)
