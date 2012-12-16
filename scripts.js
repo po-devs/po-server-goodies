0cd3f1a1a77036a9ff6b5b8d85c1cfc70bdc3e61
Benjamin Lee <kirbyfreak1998@gmail.com>
2012-12-16 15:39:59 +1100

    might remove this to be on the safe side for now

--- scripts.js
+++ scripts.js
@@ -4059,9 +4059,9 @@ modCommand: function(src, command, commandData, tar) {
         }
         return;
     }
-    if (cmp(sys.name(src),"aerith") && command == "updateplugin" && (commandData == "tours.js" || commandData == "battlefactory.js" || commandData == "newtourstats.js")) {
-        return this.ownerCommand(src, command, commandData, tar);
-    }
+    // if (cmp(sys.name(src),"aerith") && command == "updateplugin" && (commandData == "tours.js" || commandData == "battlefactory.js" || commandData == "newtourstats.js")) {
+    //    return this.ownerCommand(src, command, commandData, tar);
+    // }
     return "no command";
 },
 
