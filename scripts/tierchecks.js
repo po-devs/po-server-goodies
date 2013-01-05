1b93ca1574bbae1ba30958f13032876c7d71f654
CrystalMoogle <crystalmoogle@gmail.com>
2013-01-05 11:25:15 +0000

    Ban Kyurem-B from Wifi UU Tierchecks is temp until tiers can be updated

--- scripts/tierchecks.js
+++ scripts/tierchecks.js
@@ -259,6 +259,14 @@ tier_checker.add_new_check(INCLUDING, ["Wifi UU", "Wifi LU", "Wifi NU"], functio
     }
 });
 
+tier_check.add_new_check(INCLUDING, ["Wifi UU"], function bannedPokes(src, team, tier) { //for bans that need to be done quickly and tiers cannot be updated right away
+    for (var i = 0; i < 6; ++i) {
+        if (sys.teamPoke(src, team, i) === sys.pokeNum("Kyurem-B")){
+            return ["Kyurem-B is banned in " + tier + "."];
+        }
+    }
+});
+
 tier_checker.add_new_check(INCLUDING, ["Shanai Cup"], function shanaiAbilityCheck(src, team) {
     var bannedAbilities = {
         'treecko': ['overgrow'],
