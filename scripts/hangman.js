96b3b362d949b8af410cebdef874f2b97b430f9a
Scott <scotttm17@hotmail.co.uk>
2013-02-01 23:38:52 +0000

    rule about ruining games

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -386,6 +386,7 @@ module.exports = function () {
             "±Rules: Do not create inappropriate answers, hints or guesses.",
             "±Rules: Do not complain if another user guesses a letter, word or answer before you do.",
             "±Rules: Do not create an answer that is impossible for other people to guess, such as a personal nickname or an opinion.",
+            "±Rules: Do not spoil the answer for the rest of the channel, as that ruins the game.",
             "±Rules: All server rules apply in this channel too - type /rules to view them.",
             "±Rules: If you have doubts or think someone is breaking the rules, use /hadmins to see a list of people who may help!",
             "*** *********************************************************************** ***",
