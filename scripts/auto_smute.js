74717693595091218531d0e622800fac5adebd11
Ethan <ethanpokemononline@hotmail.com>
2013-02-03 18:29:06 -0600

    .

--- scripts/auto_smute.js
+++ scripts/auto_smute.js
@@ -8,7 +8,7 @@ module.exports.beforeChatMessage = function beforeChatMessage(src, message, chan
     var name = sys.name(src);
     var lowerM = message.toLowerCase();
     if (sys.auth(src) > 0) return;
-    if (/(the )?muted [A-Za-z]{3,10}+/i.test(name) && !sys.dbRegistered(name) && lowerM.match(/satan/gi).length > 3) {
+    if (/(the )?muted [A-Za-z]{3,10}/i.test(name) && !sys.dbRegistered(name) && lowerM.match(/satan/gi).length > 3) {
         SESSION.users(src).activate("smute", "Script", 0, "Evader", true);
         normalbot.sendAll("Evader auto-smuted: " + name + ", message: " + message, staffchannel);
         return true;
