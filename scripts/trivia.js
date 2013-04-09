4442f3098e5c9481fee0093bf0f8995b09f6e173
WriggleNightbug <intro1827@hotmail.it>
2013-04-09 22:24:41 +0300

    Added a line about how to join a game
    Additionally, the sendChanHtmlAll Psyduck colour should be closer to the normal bot colour

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -288,6 +288,7 @@ TriviaGame.prototype.startGame = function(points, name)
 	sendChanAll("»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»:",0)
 	sendChanAll("", 0);
 	this.sendAll((name != "" ? name+" started a Trivia game! " : "A trivia game was started! ") + " First to "+points+" points wins!",triviachan);
+	sendChanHtmlAll("<font color='#319d9f'><timestamp/> <b>±Psyduck:</b></font> Type <b>/join</b> to join!",triviachan);
     var players = sys.playersOfChannel(triviachan);
     // Flash players who have it enabled
     for (var p in players) {
@@ -405,10 +406,10 @@ TriviaGame.prototype.finalizeAnswers = function()
     }
     sendChanAll("", triviachan);
     var incorrectAnswers  = wrongAnswers.length > 0 ? " Incorrect answers: "+ wrongAnswers.join(", ") : "";
-    sendChanHtmlAll("<font color='#3daa68'><timestamp/> <b>±Psyduck:</b></font> Time's up!" + incorrectAnswers, triviachan);
+    sendChanHtmlAll("<font color='#319d9f'><timestamp/> <b>±Psyduck:</b></font> Time's up!" + incorrectAnswers, triviachan);
     this.sendAll("Answered correctly: " + answeredCorrectly.join(", "),triviachan);
     var x = answers.length != 1 ? "answers were" : "answer was";
-    sendChanHtmlAll("<font color='#3daa68'><timestamp/> <b>±Psyduck:</b></font> The correct "+x+": <b>"+answers.join(", ")+"</b>",triviachan);
+    sendChanHtmlAll("<font color='#319d9f'><timestamp/> <b>±Psyduck:</b></font> The correct "+x+": <b>"+answers.join(", ")+"</b>",triviachan);
 
     var leaderboard = [];
     var displayboard = [];
@@ -438,7 +439,7 @@ TriviaGame.prototype.finalizeAnswers = function()
         var w = (winners.length == 1) ? "the winner!" : "our winners!";
         winners.sort(function(a,b) { return b[1]-a[1]; });
         this.htmlAll("<h2>Congratulations to "+w+"</h2>"+winners.join(", ")+"");
-		sendChanHtmlAll("<font size=5><font color='#3daa68'><timestamp/> <b>±Psyduck: </b><font color='red'>While you're waiting for another game, why not submit a question? <a href='http://wiki.pokemon-online.eu/wiki/Community:Trivia#Submitting_Questions'>Help and Guidelines are here!</a></font></font></font>", triviachan);
+		sendChanHtmlAll("<font size=5><font color='#319d9f'><timestamp/> <b>±Psyduck: </b><font color='red'>While you're waiting for another game, why not submit a question? <a href='http://wiki.pokemon-online.eu/wiki/Community:Trivia#Submitting_Questions'>Help and Guidelines are here!</a></font></font></font>", triviachan);
         //updateLeaderboard(obj);
         this.resetTrivia();
         runUpdate();
