08b9a80fe8401126993cbfcd8c0dae2318fa2322
CrystalMoogle <crystalmoogle@gmail.com>
2013-05-26 08:20:01 +0100

    Adding start/join/round

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -1,15 +1,16 @@
 /*jshint "laxbreak":true,"shadow":true,"undef":true,"evil":true,"trailing":true,"proto":true,"withstmt":true*/
-/*global print, sys*/
+/*global print, sys, SESSION, Config, cleanFile*/
 
 //imported functions from main scripts and other plugins. Variables from here are prefixed with "mainScripts"
 var mainScripts = {
-    cleanFile:cleanFile,
-    Bot:require("bot.js").Bot,
-    dataDir: Config.dataDir
+    cleanFile: cleanFile,
+    Bot: require("bot.js").Bot,
+    dataDir: Config.dataDir,
+    createChannel: SESSION.global().channelManager.createPermChannel
 };
 
 //blackjack global defines
-var blackjackbot, deck;
+var blackjackbot, deck, blackjackchannel;
 
 var config = {
     bot: "Scrafty", //name of channel bot
@@ -19,30 +20,34 @@ var config = {
 };
 
 var blackJack = {
-    started: false,
+    phase: "",
     players: {}
 };
 
 //blackjack functions
+
 function init() {
     config = getConfig();
     blackjackbot = new mainScripts.Bot(config.bot);
+    blackjackchannel = mainScripts.createChannel(sys.channelId(config.channel), "Play Blackjack here!");
     createDeck();
 }
 
 function handleCommand(src, commandLine, channel) {
-    if (channel !== sys.channelId(config.channel)) {
+    if (channel !== blackjackchannel) {
         return false;
     }
     var returnVal = false;
     try {
         testCommand(src, commandLine, channel);
         returnVal = true;
-    } catch(e) {
+    }
+    catch (e) {
         if (e === "Command doesn't exist") {
             returnVal = false;
-        } else {
-            blackjackbot.sendMessage(src, e);
+        }
+        else {
+            blackjackbot.sendMessage(src, e, channel);
         }
     }
     return returnVal;
@@ -53,8 +58,9 @@ function testCommand(src, commandLine, channel) {
     var command, commandData;
     if (index !== -1) {
         command = commandLine.substr(0, index);
-        commandData = commandLine.substr(index+1);
-    } else {
+        commandData = commandLine.substr(index + 1);
+    }
+    else {
         command = commandLine;
     }
     if (command === "blackjackcommands" || command === "bjcommands") {
@@ -63,6 +69,10 @@ function testCommand(src, commandLine, channel) {
     }
     if (command === "start") {
         startGame();
+        return;
+    }
+    if (command === "join") {
+        joinGame(src);
         return;
     }
     if (command === "test") {
@@ -75,7 +85,7 @@ function testCommand(src, commandLine, channel) {
         if (commandData === "totals") {
             var card1 = getCard();
             var card2 = getCard();
-            var total = checkTotal([card1,card2]);
+            var total = checkTotal([card1, card2]);
             sys.sendMessage(src, card1 + " " + card2 + " " + total);
         }
         return;
@@ -85,6 +95,8 @@ function testCommand(src, commandLine, channel) {
 
 function onHelp(src, commandData, channel) {
     if (commandData === "blackjack") {
+        sys.sendMessage(src, "/bjcommands: Allows you to see the blackjack commands", channel);
+        sys.sendMessage(src, "/start: Starts a blackjack game", channel);
     }
 }
 
@@ -92,8 +104,8 @@ function createDeck() {
     deck = [];
     var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     var suits = ["♠", "♣", "♦", "♥"];
-    for(var a = 0; a < 4; a++) {
-        for(var b = 0; b < 13; b++) {
+    for (var a = 0; a < 4; a++) {
+        for (var b = 0; b < 13; b++) {
             deck.push(cards[b] + suits[a])
         }
     }
@@ -102,8 +114,8 @@ function createDeck() {
 
 function shuffle() {
     var decklength = deck.length;
-    for(var i = 0; i < 3; i++) {
-        for(var j = 0; j < decklength; j++) {
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < decklength; j++) {
             var k = sys.rand(0, decklength);
             var temp = deck[j];
             deck[j] = deck[k];
@@ -113,14 +125,15 @@ function shuffle() {
 }
 
 function getConfig() {
-    mainScripts.cleanFile(mainScripts.dataDir+"blackjack.json");
+    mainScripts.cleanFile(mainScripts.dataDir + "blackjack.json");
     var configFile = sys.getFileContent(mainScripts.dataDir + "blackjack.json");
     if (configFile === "") {
         return config;
     }
     try {
         return JSON.parse(configFile)
-    } catch (e) {
+    }
+    catch (e) {
         print("Error in config:" + e);
         return config;
     }
@@ -129,30 +142,31 @@ function getConfig() {
 function getCard() {
     var card = deck[0];
     deck.push(deck.shift());
-    return card
+    return card;
 }
 
 function checkTotal(cards) {
     var fcards = ["J", "Q", "K"];
     var ace = 0;
     var total = 0;
-    for(var y = 0; y < cards.length; y++) {
+    for (var y = 0; y < cards.length; y++) {
         var cardadd;
-        if(cards[y][1] == "0") {
+        if (cards[y][1] == "0") {
             cardadd = cards[y][0] + cards[y][1];
-        } else {
+        }
+        else {
             cardadd = cards[y][0];
         }
-        if(fcards.indexOf(cardadd) !== -1) {
+        if (fcards.indexOf(cardadd) !== -1) {
             cardadd = 10;
         }
-        if(cardadd == "A") {
+        if (cardadd == "A") {
             ace = ace + 1;
             cardadd = 11;
         }
         total = total + parseInt(cardadd)
     }
-    while(ace > 0 && total > 21) {
+    while (ace > 0 && total > 21) {
         ace = ace - 1;
         total = total - 10;
     }
@@ -160,16 +174,65 @@ function checkTotal(cards) {
 }
 
 function startGame() {
-    if (blackJack.started === true) {
+    if (blackJack.phase !== "") {
         throw "Game has already started";
     }
-    blackjackbot.sendAll("A new blackjack game has started!", config.channel);
-    blackjackbot.sendAll("You have 30 seconds to join!", config.channel);
-    blackJack.started = true;
+    blackjackbot.sendAll("A new blackjack game has started!", blackjackchannel);
+    blackjackbot.sendAll("You have 30 seconds to join!", blackjackchannel);
+    blackJack.phase = "joining";
+    sys.setTimer(function () {
+        startRound()
+    }, 30000, false);
 }
+
+function joinGame(src) {
+    if (blackJack.phase !== "joining") {
+        throw "You cannot join now!";
+    }
+    if (blackJack.players.hasOwnProperty(src)) {
+        throw "You already joined!";
+    }
+    blackJack.players[src] = {
+        name: sys.name(src),
+        cards: [],
+        out: false,
+        total: 0,
+        type: "normal"
+    };
+    blackjackbot.sendAll(sys.name(src) + " joined the game!");
+}
+
+function startRound() {
+    if (blackJack.phase !== "joining") {
+        return;
+    }
+    if (Object.keys(blackJack.players).length < 1) {
+        blackjackbot.sendAll("Not enough players", blackjackchannel);
+        return;
+    }
+    blackJack.phase = "playing";
+    for (var x in blackJack.players) {
+        if (blackJack.players.hasOwnProperty(x)) {
+            var player = blackJack.players[x];
+            var name = player.name;
+            var cards = player.cards.push(getCard(), getCard());
+            var total = checkTotal(cards);
+            player.total = total;
+            blackjackbot.sendAll(name + "'s cards are " + cards + ". Total: " + total + ".");
+            if (total == 21) {
+                blackjackbot.sendAll(name + " got blackjack!");
+                player.type = "blackjack";
+                player.out = true;
+                //checkGame();
+            }
+        }
+    }
+}
+
+
 //exports to main script
 module.exports = {
-    init:init,
-    handleCommand:handleCommand,
-    onHelp:onHelp
+    init: init,
+    handleCommand: handleCommand,
+    onHelp: onHelp
 };
