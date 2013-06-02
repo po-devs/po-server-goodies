fa72f0d1de5635ab7f824f05cbb01faacf981ae1
CrystalMoogle <crystalmoogle@gmail.com>
2013-06-02 09:04:49 +0100

    RAINBOW

--- scripts.js
+++ scripts.js
@@ -2375,7 +2375,7 @@ userCommand: function(src, command, commandData, tar) {
            sendChanHtmlAll("<font color='"+colour+"'><timestamp/> *** <b>" + utilities.html_escape(sys.name(src)) + "</b> " + messagetosend + "</font>", channel);
         } else if (command == "rainbow" && SESSION.global().allowRainbow && channel !== 0 && channel !== tourchannel && channel !== mafiachan && channel != sys.channelId("Trivia")) {
             var auth = 1 <= sys.auth(src) && sys.auth(src) <= 3;
-            var colours = ["red", "blue", "yellow", "cyan", "black", "orange", "green", "#FF0000", "#FF5A00", "#A5ff00", "#00ff5A", "#0000ff", "#FF00B4", "#FFff00"];
+            var colours = ["#FF0000", "#FFDB00", "#FFff00", "#24ff00", "#00ff00", "#00ffDB", "#00ffff"];
             var randColour = function() { return colours[sys.rand(0,colours.length-1)]; };
             var toSend = ["<timestamp/><b>"];
             if (auth) toSend.push("<span style='color:" + randColour() + "'>+</span><i>");
