4b3984ad1515bbee6cd8383e25ec26fc7293cc59
CrystalMoogle <crystalmoogle@gmail.com>
2013-07-04 06:52:01 +0100

    Reg limit

--- scripts.js
+++ scripts.js
@@ -54,7 +54,7 @@ var Config = {
     canJoinStaffChannel: ["Lamperi-", "Peanutsdroid", "QuX", "Ethan-"],
     disallowStaffChannel: [],
     topic_delimiter: " | ",
-    registeredLimit: 10
+    registeredLimit: 0
 };
 
 // Don't touch anything here if you don't know what you do.
