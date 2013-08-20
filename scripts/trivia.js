62f4e8738aff5e9cd7894d9c15b9d2320d398817
WriggleNightbug <intro1827@hotmail.it>
2013-08-20 13:18:40 +0200

    Accounting for changing a category

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -17,7 +17,7 @@ var utilities = require("utilities.js");
 var triviachan, revchan;
 var triviabot = new Bot("Psyduck");
 
-var triviaCategories = ['Anime/Manga', 'Animals', 'Art', 'Comics', 'Food/Drink', 'Games', 'Geography', 'History', 'Internet', 'Language', 'Literature', 'Math', 'Misc', 'Movies', 'Music', 'Mythology', 'PO', 'Pokemon', 'Politics', 'Religion', 'Science', 'Social Science', 'Society', 'Space', 'Sports', 'Technology', 'TV', 'Video Games'];
+var triviaCategories = ['Anime/Manga', 'Animals', 'Art', 'Comics', 'Food/Drink', 'Games', 'Geography', 'History', 'Internet', 'Language', 'Literature', 'Math', 'Misc', 'Movies', 'Music', 'Mythology', 'Pokemon', 'Pokemon Online', 'Politics', 'Religion', 'Science', 'Social Science', 'Society', 'Space', 'Sports', 'Technology', 'TV', 'Video Games'];
 var lastCatGame = 0;
 var lastUsedCats = [];
 var suggestion = {};
