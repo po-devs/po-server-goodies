7763595a0d72c30b44e4fb39c95b788357118355
CrystalMoogle <crystalmoogle@gmail.com>
2013-10-02 10:26:09 +0100

    Add gen parameter so sprites appear on source built client

--- scripts/usercommands.js
+++ scripts/usercommands.js
@@ -696,7 +696,7 @@ exports.handleCommand = function(src, command, commandData, tar, channel) {
         var levels = [5, 50, 100];
         sys.sendHtmlMessage(src, "", channel);
         sys.sendHtmlMessage(src, "<b><font size = 4>" + sys.pokemon(pokeId) + "</font></b>", channel);
-        sys.sendHtmlMessage(src, "<img src='pokemon:num=" + pokeId + "'><img src='pokemon:num=" + pokeId + "&shiny=true'>", channel);
+        sys.sendHtmlMessage(src, "<img src='pokemon:num=" + pokeId + "&gen=5'><img src='pokemon:num=" + pokeId + "&shiny=true&gen=5'>", channel);
         sys.sendHtmlMessage(src, "<b>Type:</b> " + type1 + (type2 === "???" ? "" : "/" + type2), channel);
         sys.sendHtmlMessage(src, "<b>Abilities:</b> " + ability1 + (ability2 === "(No Ability)" ? "" : ", " + ability2) + (ability3 === "(No Ability)" ? "" : ", " + ability3 + " (Dream World)"), channel);
         sys.sendHtmlMessage(src, "<b>Height:</b> " + getHeight(pokeId) + " m", channel);
