e99554a31797a45a7f3b4316954906b48b206e37
WriggleNightbug <intro1827@hotmail.it>
2013-10-20 15:30:30 +0200

    Added Pokédex number to /pokemon

--- scripts/usercommands.js
+++ scripts/usercommands.js
@@ -695,7 +695,7 @@ exports.handleCommand = function(src, command, commandData, tar, channel) {
         var stats = ["HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"];
         var levels = [5, 50, 100];
         sys.sendHtmlMessage(src, "", channel);
-        sys.sendHtmlMessage(src, "<b><font size = 4>" + sys.pokemon(pokeId) + "</font></b>", channel);
+        sys.sendHtmlMessage(src, "<b><font size = 4># " + pokeId % 65536 + " " + sys.pokemon(pokeId) + "</font></b>", channel);
         sys.sendHtmlMessage(src, "<img src='pokemon:num=" + pokeId + "&gen=5'><img src='pokemon:num=" + pokeId + "&shiny=true&gen=5'>", channel);
         sys.sendHtmlMessage(src, "<b>Type:</b> " + type1 + (type2 === "???" ? "" : "/" + type2), channel);
         sys.sendHtmlMessage(src, "<b>Abilities:</b> " + ability1 + (ability2 === "(No Ability)" ? "" : ", " + ability2) + (ability3 === "(No Ability)" ? "" : ", " + ability3 + " (Dream World)"), channel);
