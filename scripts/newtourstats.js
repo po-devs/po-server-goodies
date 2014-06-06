1506a275ec6490960d24f3b22f3027db5f09db46
Sulcata <Sulcata@users.noreply.github.com>
2014-06-06 18:57:36 -0400

    Battle Factories -> C Tier
    Diamonds, celes, and I discussed and agreed on this.

--- scripts/newtourstats.js
+++ scripts/newtourstats.js
@@ -534,8 +534,8 @@ function awardTourPoints(player, size, tier, delim, place) {
     }
     var tiers_a = [];
     var tiers_b = []; // default
-    var tiers_c = ["Monotype", "Sky Battle", "Battle Factory", "Battle Factory 6v6"];
-    var tiers_d = ["Challenge Cup", "Inverted Challenge Cup"];
+    var tiers_c = ["Monotype", "Sky Battle", "XY Cup"];
+    var tiers_d = ["Challenge Cup", "Inverted Challenge Cup", "Battle Factory", "Battle Factory 6v6"];
     var tiers_e = ["Wifi CC 1v1", "XY 1v1", "Random Battle"];
     var tiers_f = ["CC 1v1"];
     var tiers_z = ["Metronome", "Red/Blue", "Yellow", "Stadium", "Stadium w/ Tradebacks",
