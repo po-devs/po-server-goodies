587f4b0f9edf90e0d088812cb9f7fae61724b937
Kase0786 <ksingh.19.19@gmail.com>
2014-11-15 13:00:42 +1100

    Update auto_smute.js

--- scripts/auto_smute.js
+++ scripts/auto_smute.js
@@ -1,12 +1,9 @@
 /*global normalbot, script, sys, SESSION, module, staffchannel, autosmute, sendChanAll */
 //inherited from main script
+module.exports.init = function () {}; //this is just so it says when updated thought /updateplugin :x
+module.exports.beforeChatMessage = function beforeChatMessage(src, message, channel) {};
 
-module.exports.init = function() {}; //this is just so it says when updated thought /updateplugin :x
-
-module.exports.beforeChatMessage = function beforeChatMessage(src, message, channel) {
-};
-
-module.exports.afterChangeTeam = function afterChangeTeam(src){
+module.exports.afterChangeTeam = function afterChangeTeam(src) {
     this.afterLogIn(src);
 };
 
@@ -17,7 +14,7 @@ module.exports.afterLogIn = function afterLogIn(src) {
     if (sys.auth(src) > 0 || SESSION.users(src).smute.active) {
         return;
     }
-    /*if (sys.getColor(src) == "#ff007f" && /doj/i.test(sys.name(src))) {
+/*if (sys.getColor(src) == "#ff007f" && /doj/i.test(sys.name(src))) {
         normalbot.sendAll("Smute based on color: " + sys.name(src) + ", IP: " + sys.ip(src), staffchannel);
         var endtime = parseInt(sys.time(), 10) + 86400;
         SESSION.users(src).activate("smute", "Script", endtime, "User is probably Doj; color based auto smute", true);
