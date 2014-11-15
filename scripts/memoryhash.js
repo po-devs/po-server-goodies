1783e1c6f28a72243febc0fa2a80c69cf7a2db4f
Kase0786 <ksingh.19.19@gmail.com>
2014-11-15 13:39:47 +1100

    Update memoryhash.js

--- scripts/memoryhash.js
+++ scripts/memoryhash.js
@@ -11,50 +11,44 @@
  *
  *  All keys and values must be strings.
  */
-function MemoryHash(filename)
-{
+function MemoryHash(filename) {
     this.hash = {};
     this.fname = filename;
 
     var contents = sys.getFileContent(this.fname);
     if (contents !== undefined) {
         var lines = contents.split("\n");
-        for(var i = 0; i < lines.length; ++i) {
+        for (var i = 0; i < lines.length; ++i) {
             var line = lines[i];
             var key_value = line.split("*");
             var key = key_value[0];
             var value = key_value.slice(1).join("*");
             if (key.length > 0) {
-                if (value === undefined)
-                    value = '';
+                if (value === undefined) value = '';
                 this.hash[key] = value;
             }
         }
     }
 }
 
-MemoryHash.prototype.add = function(key, value)
-{
+MemoryHash.prototype.add = function (key, value) {
     this.hash[key] = value;
     // it doesn't matter if we add a duplicate,
     // when we remove something eventually,
     // duplicates will be deleted
-    sys.appendToFile(this.fname, key +'*' + value + '\n');
+    sys.appendToFile(this.fname, key + '*' + value + '\n');
 }
 
-MemoryHash.prototype.get = function(key)
-{
+MemoryHash.prototype.get = function (key) {
     return this.hash[key];
 }
 
-MemoryHash.prototype.remove = function(key)
-{
+MemoryHash.prototype.remove = function (key) {
     delete this.hash[key];
     this.save();
 }
 
-MemoryHash.prototype.removeIf = function(test)
-{
+MemoryHash.prototype.removeIf = function (test) {
     var i;
     var toDelete = []
     for (i in this.hash) {
@@ -67,24 +61,21 @@ MemoryHash.prototype.removeIf = function(test)
     }
 }
 
-MemoryHash.prototype.clear = function()
-{
+MemoryHash.prototype.clear = function () {
     this.hash = {};
     this.save();
 }
 
-MemoryHash.prototype.save = function()
-{
+MemoryHash.prototype.save = function () {
     var lines = [];
     for (var i in this.hash) {
-        lines.push(i +'*' + this.hash[i] + '\n');
+        lines.push(i + '*' + this.hash[i] + '\n');
     }
     sys.writeToFile(this.fname, lines.join(""))
 }
 
-MemoryHash.prototype.escapeValue = function(value)
-{
-    return value.replace(/\*\n/g,'');
+MemoryHash.prototype.escapeValue = function (value) {
+    return value.replace(/\*\n/g, '');
 }
 
 /* End of prototype MemoryHash */
