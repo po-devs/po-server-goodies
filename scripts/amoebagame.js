a766a158cd80ce678cc8fc287c528886ed8be9a2
Kase0786 <ksingh.19.19@gmail.com>
2014-11-15 12:59:55 +1100

    Update amoebagame.js

--- scripts/amoebagame.js
+++ scripts/amoebagame.js
@@ -1,14 +1,14 @@
 /*
  * amoebagame.js
  */
-module.exports = function() {
+module.exports = function () {
     var id;
     var name = "Evolution Game";
     var species = ['Solosis', 'Lilligant', 'Krokorok', 'Minccino', 'Mankey', 'Arceus'];
     var messages = ['solosis solosis', 'lilli-lilligant', 'krokokokokorok!', 'minccino?', 'ma-mankey mankey!', '-'];
     var battles = {};
 
-    var init = function() {
+    var init = function () {
         if (sys.existChannel(name)) {
             id = sys.channelId(name);
         } else {
@@ -19,22 +19,18 @@ module.exports = function() {
         SESSION.channels(id).master = "lamperi";
     };
 
-    var beforeChatMessage = function(src, message, chan) {
+    var beforeChatMessage = function (src, message, chan) {
         if (chan != id) return false;
         var role = SESSION.users(src).amoeba.role;
-        if (sys.auth(src) > 0 && ["/","!"].indexOf(message[0]) > -1) return false;
+        if (sys.auth(src) > 0 && ["/", "!"].indexOf(message[0]) > -1) return false;
         if (role != species.length - 1) {
             sendChanAll(sys.name(src) + ": " + messages[role], id);
             if (battles[role] && battles[role] != src && sys.isInChannel(battles[role], id)) {
-                var winner = sys.rand(0,2) > 0 ? battles[role] : src;
+                var winner = sys.rand(0, 2) > 0 ? battles[role] : src;
                 var loser = winner == src ? battles[role] : src;
                 SESSION.users(winner).amoeba.role++;
                 if (SESSION.users(loser).amoeba.role > 0) SESSION.users(loser).amoeba.role--;
-                sendChanAll("+EvolutionBot: Two " + species[role] + "s, "
-                    + sys.name(battles[role]) + " and " + sys.name(src) + " engaged in a fierce battle! "
-                    + sys.name(winner) + " won and is now a " + species[SESSION.users(winner).amoeba.role] + ". "
-                    + sys.name(loser) + " lost and is now a " + species[SESSION.users(loser).amoeba.role] + ".",
-                id);
+                sendChanAll("+EvolutionBot: Two " + species[role] + "s, " + sys.name(battles[role]) + " and " + sys.name(src) + " engaged in a fierce battle! " + sys.name(winner) + " won and is now a " + species[SESSION.users(winner).amoeba.role] + ". " + sys.name(loser) + " lost and is now a " + species[SESSION.users(loser).amoeba.role] + ".", id);
                 delete battles[role];
             } else {
                 battles[role] = src;
@@ -45,7 +41,7 @@ module.exports = function() {
         return false;
     }
 
-    var beforeChannelJoin = function(src, channel) {
+    var beforeChannelJoin = function (src, channel) {
         if (channel != id) return false;
         SESSION.users(src).amoeba = {};
         SESSION.users(src).amoeba.role = 0;
