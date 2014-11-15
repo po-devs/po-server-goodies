c32b103db24af72685a40d2f6f615a6ca6c15018
Kase0786 <ksingh.19.19@gmail.com>
2014-11-15 13:11:24 +1100

    Update bot.js

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,40 +1,28 @@
 function Bot(name) {
     this.name = name;
 }
-Bot.prototype.formatMsg = function(message)
-{
+Bot.prototype.formatMsg = function (message) {
     return "±" + this.name + ": " + message;
-}
-/* Shortcuts to sys functions */
-Bot.prototype.sendAll = function(message, channel)
-{
-    if (channel === undefined || channel == -1)
-        sendChanAll(this.formatMsg(message),-1);
-    else
-        sendChanAll(this.formatMsg(message), channel);
+} /* Shortcuts to sys functions */
+Bot.prototype.sendAll = function (message, channel) {
+    if (channel === undefined || channel == -1) sendChanAll(this.formatMsg(message), -1);
+    else sendChanAll(this.formatMsg(message), channel);
 }
 
-Bot.prototype.sendMessage = function(tar, message, channel)
-{
-    if (channel === undefined)
-        sys.sendMessage(tar, this.formatMsg(message));
-    else
-        sys.sendMessage(tar, this.formatMsg(message), channel);
+Bot.prototype.sendMessage = function (tar, message, channel) {
+    if (channel === undefined) sys.sendMessage(tar, this.formatMsg(message));
+    else sys.sendMessage(tar, this.formatMsg(message), channel);
 }
 
-Bot.prototype.sendMainTour = function(message)
-{
+Bot.prototype.sendMainTour = function (message) {
     this.sendAll(message, 0);
     // Relies on Tournaments channel
     this.sendAll(message, sys.channelId("Tournaments"));
-}
-/* Following two rely on global channel parameter */
-Bot.prototype.sendChanMessage = function(tar, message)
-{
+} /* Following two rely on global channel parameter */
+Bot.prototype.sendChanMessage = function (tar, message) {
     this.sendMessage(tar, message, channel);
 }
-Bot.prototype.sendChanAll = function(message)
-{
+Bot.prototype.sendChanAll = function (message) {
     this.sendAll(message, channel);
 }
 
