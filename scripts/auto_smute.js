6cd68c0a70e26a7c5ff2fe8e67752056be95c7c3
Kase0786 <ksingh.19.19@gmail.com>
2014-11-15 13:02:15 +1100

    Update auto_smute.js

--- scripts/auto_smute.js
+++ scripts/auto_smute.js
@@ -14,7 +14,7 @@ module.exports.afterLogIn = function afterLogIn(src) {
     if (sys.auth(src) > 0 || SESSION.users(src).smute.active) {
         return;
     }
-/*if (sys.getColor(src) == "#ff007f" && /doj/i.test(sys.name(src))) {
+    /*if (sys.getColor(src) == "#ff007f" && /doj/i.test(sys.name(src))) {
         normalbot.sendAll("Smute based on color: " + sys.name(src) + ", IP: " + sys.ip(src), staffchannel);
         var endtime = parseInt(sys.time(), 10) + 86400;
         SESSION.users(src).activate("smute", "Script", endtime, "User is probably Doj; color based auto smute", true);
