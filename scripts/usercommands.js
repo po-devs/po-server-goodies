c48849f6afd7552cac315b3dfd7187be9670b178
PokeWorld BW <PokeWorldDP@gmail.com>
2015-06-25 03:15:07 -0400

    Fix spacing?

--- scripts/usercommands.js
+++ scripts/usercommands.js
@@ -368,21 +368,21 @@ exports.handleCommand = function(src, command, commandData, tar, channel) {
         SESSION.channels(channel).setTopic(src, topic.join(Config.topic_delimiter));
         return;
     }
-	if (command == "topicparts") {
-		var topic = SESSION.channels(channel).topic;
-		if (typeof topic == "string" && topic !== "") {
-			var i = 0;
-			topic = topic.split(" | ").map(function(part) { 
-				return "<font color='blue'><b>[" + ++i + "]</b></font> " + part 
-			}).join(" | ");
-			// HTML isn't necessary, but it makes the number more obvious
-			sys.sendHtmlMessage(src, "<font color='#3DAA68'><timestamp/> <b>±" + Config.channelbot + ":</b></font> Topic for this channel is: " + topic, channel);
-			return;
-		} else {
-			channelbot.sendMessage(src, "No topic set for this channel.", channel);
-			return;
-		};
-	}
+    if (command == "topicparts") {
+        var topic = SESSION.channels(channel).topic;
+        if (typeof topic == "string" && topic !== "") {
+            var i = 0;
+            topic = topic.split(" | ").map(function(part) { 
+                return "<font color='blue'><b>[" + ++i + "]</b></font> " + part 
+            }).join(" | ");
+            // HTML isn't necessary, but it makes the number more obvious
+            sys.sendHtmlMessage(src, "<font color='#3DAA68'><timestamp/> <b>±" + Config.channelbot + ":</b></font> Topic for this channel is: " + topic, channel);
+            return;
+        } else {
+            channelbot.sendMessage(src, "No topic set for this channel.", channel);
+            return;
+        };
+    }
     if (command == "uptime") {
         if (typeof(script.startUpTime()) != "string") {
             countbot.sendMessage(src, "Somehow the server uptime is messed up...", channel);
