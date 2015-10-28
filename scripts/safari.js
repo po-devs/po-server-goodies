c4b03df405c7ef2f47d038777c260ec5ff642002
Fuzzysqurl <risuke@comcast.net>
2015-10-28 01:56:17 -0500

    idk anymore

--- scripts/safari.js
+++ scripts/safari.js
@@ -1446,7 +1446,7 @@ function Safari() {
             return true;
         }
         if (command === "view") {
-            if (commandData) {
+            if (commandData.length > 1) {
                 safari.viewPlayer(src, commandData);
             } else {
                 safari.viewOwnInfo(src, commandData);
