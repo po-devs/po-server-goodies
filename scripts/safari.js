235cf02269ef95d8354ae720ac106bc0fa6c8aa6
Fuzzysqurl <risuke@comcast.net>
2015-10-30 00:49:16 -0500

    Swap code blocks

--- scripts/safari.js
+++ scripts/safari.js
@@ -1659,14 +1659,14 @@ function Safari() {
             return true;
         }
         if (command === "info") {
-            if (contestCount > 0) {
-                var min = Math.floor(contestCooldown/60);
-                var sec = contestCooldown%60;
-                safaribot.sendMessage(src, "Time until next Contest: " + min + " minutes, " + sec + " seconds.", safchan);
-            } else {
+            if (contestCount > 0) {                
                 var min = Math.floor(contestCount/60);
                 var sec = contestCount%60;
                 safaribot.sendMessage(src, "Time until the Contest ends: " + min + " minutes, " + sec + " seconds.", safchan);
+            } else {
+                var min = Math.floor(contestCooldown/60);
+                var sec = contestCooldown%60;
+                safaribot.sendMessage(src, "Time until next Contest: " + min + " minutes, " + sec + " seconds.", safchan);
             }
             safaribot.sendMessage(src, "Current Gachapon Jackpot: " + Math.floor(gachaJackpot/10) + ".", safchan);
             return true;
