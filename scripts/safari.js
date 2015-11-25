a3be93b2ae7725c4896c752cd3f4201c295fa66c
Fuzzysqurl <risuke@comcast.net>
2015-11-25 13:47:10 -0600

    Actually kill mods.

--- scripts/safari.js
+++ scripts/safari.js
@@ -5160,7 +5160,7 @@ function Safari() {
         if (channel !== safchan && [].indexOf(command) === -1) {
             return false;
         }
-        if (SESSION.channels(safchan).muteall && !SESSION.channels(safchan).isChannelOwner(src) && sys.auth(src) === 0) {
+        if (SESSION.channels(safchan).muteall && !SESSION.channels(safchan).isChannelOwner(src)) {
             safaribot.sendMessage(src, "You can't play Safari while the channel is silenced.", safchan);
             return true;
         }
