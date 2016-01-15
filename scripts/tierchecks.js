2967036edcd80aa79fc1baf249d5c2d6037b79ae
Fuzzysqurl <risuke@comcast.net>
2016-01-15 13:19:15 -0600

    ,

--- scripts/tierchecks.js
+++ scripts/tierchecks.js
@@ -351,7 +351,7 @@ tier_checker.add_new_check(INCLUDING, ["Monotype"], function monotypeCheck(src, 
     //  type: {type: "Type", items: [], pokes: [], abilities: []}
     var typeBans = { 
         flying: {type: "Flying", items: ["Charizardite X"]},
-        ghost: {type: "Ghost", pokes: ["Aegislash"]}
+        steel: {type: "Steel", pokes: ["Aegislash"]}
     };
     var ret = [], item, pkmn;
     for (var e in typeBans) {
