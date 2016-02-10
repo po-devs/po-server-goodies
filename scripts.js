16c60478342e290b598a3f3b04f2250a73497e1c
Arburator <garyoakheart@gmail.com>
2016-02-10 12:11:27 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1884,7 +1884,7 @@ beforeChatMessage: function(src, message, chan) {
 	return;
     }
     if (sys.name(src) == "Cold Wind") {
-    	sys.sendHtmlAll("<span style 'color: #55ffff'><timestamp/><b>( Owner ) Cold Wind: </b></span><font size=3 font color=red>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+    	sys.sendHtmlAll("<span style 'color: #34ced2'><timestamp/><b>( Owner ) Cold Wind: </b></span><font size=3 font color=red>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
     	sys.stopEvent();
     	this.afterChatMessage(src, message, channel);
     	return;
