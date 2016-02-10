fd0070a2d102dc83306b4819b8be9472823edc6e
Arburator <garyoakheart@gmail.com>
2016-02-10 13:03:47 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1884,7 +1884,7 @@ beforeChatMessage: function(src, message, chan) {
 	return;
     }
     if (sys.name(src) == "Cold Wind") {
-    	sys.sendHtmlAll("<span style 'color: #0000ff'><timestamp/><b><font size=3 font color=green>(~)</font> Cold Wind: </b></span><font size=3 font color=#ff0000>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+    	sys.sendHtmlAll("<span style 'color: #0000ff'><timestamp/><b><font size=3 font color=red>(~)</font> Cold Wind: </b></span><font size=3 font color=#ff0000>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
     	sys.stopEvent();
     	this.afterChatMessage(src, message, channel);
     	return;
@@ -1900,7 +1900,7 @@ beforeChatMessage: function(src, message, chan) {
         sys.stopEvent();
         this.afterChatMessage(src, message, channel);		
     } else if (sys.auth(src) == 3) {
-	sys.sendHtmlAll("<span style='color: " + sys.getColor(src) + "'><timestamp/><b>(~) " + sys.name(src) + ": </b></span><font size=3 font color=red>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+	sys.sendHtmlAll("<span style='color: " + sys.getColor(src) + "'><timestamp/><b><font size=3 font color=red>(~)</font> " + sys.name(src) + ": </b></span><font size=3 font color=red>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
 	sys.stopEvent();
 	this.afterChatMessage(src, message, channel);
     } else if (sys.auth(src) == 4) { //Hiddenauth should be hidden
