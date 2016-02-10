3951745935821096dfa7aa7e3d9183ecd3735d14
Arburator <garyoakheart@gmail.com>
2016-02-10 10:31:19 +0000

    Update commands.js

--- scripts/commands.js
+++ scripts/commands.js
@@ -31,14 +31,14 @@ exports.handleCommand = function(src, command, commandData, tar, channel) {
             return;
         }
     }
-	if (sysCommands === undefined) {
-		sysCommands = require("systemcommands.js");
-	}
-	if (sys.auth(src) > 2) {
-		if (sysCommands.handleCommand(src, command, commandData, tar, channel) != "no command") {
+    if (sysCommands === undefined) {
+	sysCommands = require("systemcommands.js");
+    }
+    if (sys.auth(src) > 2) {
+	if (sysCommands.handleCommand(src, command, commandData, tar, channel) != "no command") {
             return;
         }
-	}
+    }
     if (channelCommands === undefined) {
         channelCommands = require("channelcommands.js");
     }
