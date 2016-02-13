44a7c151b8a02f6cd1f5c5b17defc3783acb7758
Xotic <pokemonbattlesimulator@gmail.com>
2016-02-13 16:31:15 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1348,9 +1348,9 @@ afterLogIn : function(src) {
     } else if (sys.auth(src) == 3) {
     	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=2 font color=#00007f>(~)</font> " + sys.name(src) + "</span><font size=2 font color=black> Joined the channel!", 0);
     } else if (sys.auth(src) == 4) {
-    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=2 font color=#005500></font> " + sys.name(src) + "</span><font size=2 font color=black> to the server!", 0);
+    	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=2 font color=#005500></font> " + sys.name(src) + "</span><font size=2 font color=black> Joined the channel!", 0);
     } else {
-        sys.sendHtmlAll("<timestamp/><style='color: " + sys.getColor(src) + "'><b><font size=2 font color=#005500></font> " + sys.name(src) + "</span><font size=2 font color=black> Joined the channel!", 0);
+        sys.sendHtmlAll("<timestamp/><style='color: " + sys.getColor(src) + "'><b><font size=2 font color=#005500></font> " + sys.name(src) + "</span><font size=2 font color=black> Joined the channel! Register to keep your account secure.", 0);
     }
     sys.sendMessage(src, "");
 
