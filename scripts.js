905a2d4b58cf960bce0ed9606d497c857523731e
salami7e <salami7e@gmail.com>
2016-02-16 00:37:39 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1964,7 +1964,7 @@ beforeChatMessage: function(src, message, chan) {
     	return;
     }
     if (sys.name(src) == "Adam") {
-	sys.sendHtmlAll("<span style='color: #3500b8'><timestamp/><b><font size=3 font color=red><img src='pokemon:num=491&gen=6' height=45></font> Adam: </b></span><font size=3 font color=purple>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+	sys.sendHtmlAll("<span style='color: #3500b8'><timestamp/><b><font size=3 font color=red><img src='pokemon:num=491&gen=6' height=50></font> Adam: </b></span><font size=3 font color=purple>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
         sys.stopEvent();
 	this.afterChatMessage(src, message, channel);
 	return;
