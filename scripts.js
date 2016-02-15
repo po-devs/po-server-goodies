4e86592d54293c2a8ed546b602cfc977c17fdcc0
Xotic <pokemonbattlesimulator@gmail.com>
2016-02-15 18:28:32 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1977,14 +1977,8 @@ beforeChatMessage: function(src, message, chan) {
     	this.afterChatMessage(src, message, channel);
     	return;
     }*/
-    if (sys.name(src) == "[TR]Aranian25") {
-    	sys.sendHtmlAll("<span style 'color: #ff0000'><timestamp/><b><font size=3 font color=green>(Expert)</font> [TR]Aranian25: </b></span><font size=3 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
-    	sys.stopEvent();
-    	this.afterChatMessage(src, message, channel);
-    	return;
-    }
     if (sys.name(src) == "Master") {
-    	sys.sendHtmlAll("<span style 'color: " + sys.getColor(src) + "'><timestamp/></td><font size=3 font color=blue><img src='item:274' title='Money'></font> " + sys.name(src) + ": </b></span><font size=3 font color=black>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+    	sys.sendHtmlAll("<span style 'color: #ff0000'><timestamp/><b><font size=3 font color=green>(<img src='pokemon:num=025&gen=1'>)</font> Master: </b></span><font size=3 font color=blue>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
     	sys.stopEvent();
     	this.afterChatMessage(src, message, channel);
     	return;
