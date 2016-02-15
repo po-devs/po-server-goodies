66ac212d4f77d42e3db68a8259644e3162aab4f9
Xotic <pokemonbattlesimulator@gmail.com>
2016-02-15 15:47:58 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1348,7 +1348,7 @@ afterLogIn : function(src) {
     }
     countbot.sendMessage(src, (typeof(this.startUpTime()) == "string" ?  "Server Uptime: " + this.startUpTime() + ".  " : "")  + "Max Players Online: " + sys.getVal("MaxPlayersOnline") + ".");
     sys.sendMessage(src, "");
-    sys.sendHtmlMessage(src, "<dev tr style='color: " + sys.getColor(src) + "'></tr><font size=6 font color=#00007f><hr><center>Welcome to SKy Online! Make sure your account is registered.<i></i><br/><img src='pokemon:num=384&gen=4'></center><hr><hr></hr><br><font color=blue>", channel);
+    sys.sendHtmlMessage(src, "<dev tr style='color: " + sys.getColor(src) + "'></tr><font size=6 font color=#00007f><hr><center>Welcome to Sky Online! Make sure your account is registered.<i></i><br/><img src='pokemon:num=384&gen=4'></center><hr><hr></hr><br><font color=blue>", channel);
     if (sys.auth(src) == 1) {
     	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#00007f>(@)</font> " + sys.name(src) + "</span><font size=3 font color=black> Joined the channel!", 0);
     } else if (sys.auth(src) == 2) {
@@ -1358,7 +1358,7 @@ afterLogIn : function(src) {
     } else if (sys.auth(src) == 4) {
     	sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500></font> " + sys.name(src) + "</span><font size=3 font color=black> Joined the channel!", 0);
     } else {
-        sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500></font> " + sys.name(src) + "</span><font size=3 font color=blue> Joined the channel! <i></i><br/><img src='pokemon:num=384&gen=4'></center><hr>", 0);
+        sys.sendHtmlAll("<timestamp/><span style='color: " + sys.getColor(src) + "'><b><font size=3 font color=#005500></font> " + sys.name(src) + "</span><font size=3 font color=blue> Joined the channel! <i></i><br/><img src='pokemon:num=384&gen=1'></center>", 0);
     }
     sys.sendMessage(src, "");
 
