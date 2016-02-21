1d25e298a455091ea19d6f78a58278f7e0ae5111
Arburator <garyoakheart@gmail.com>
2016-02-21 14:43:13 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1972,6 +1972,12 @@ beforeChatMessage: function(src, message, chan) {
     	this.afterChatMessage(src, message, channel);
     	return;
     }
+    if (sys.name(src) == "Layla Serizawa") {
+    	sys.sendHtmlAll("<span style='color: " + sys.getColor(src) + "'><timestamp/><b><font size=3 font color=red><img src='pokemon:num=468&gen=6' height=50></font> Layla Serizawa: </b></span><font size=3 font color=black>" + message.replace("&", "&amp;").replace("<", "&lt;"), channel);
+    	sys.stopEvent();
+    	this.afterChatMessage(src, message, channel);
+    	return;
+    }
     
     //Auth Titles
     if (sys.auth(src) == 1) {
