5c43e66ab9c03ff4b082c0af9b4e5f5de116aa06
Strudels <StrudelsPO@gmail.com>
2016-10-24 22:03:54 +0800

    data[1]

--- scripts/modcommands.js
+++ scripts/modcommands.js
@@ -40,7 +40,7 @@ exports.handleCommand = function (src, command, commandData, tar, channel) {
             }
             var channelData = {'type': 'ChannelUsers', 'channel-id': chanid, 'channel-name': chanName, 'players': objectList};
             if (filterOS) {
-                channelData[data1] = data1;
+                channelData[data[1]] = data[1];
             }
             sys.sendMessage(src, "+ChannelUsers:"+JSON.stringify(channelData), channel);
         }
