473888207012f4d1eb05cc883ca5387cbaedf75d
Strudels <StrudelsPO@gmail.com>
2016-11-16 01:18:25 +0800

    Blinky

--- scripts/modcommands.js
+++ scripts/modcommands.js
@@ -512,7 +512,8 @@ exports.handleCommand = function (src, command, commandData, tar, channel) {
                 'bans' : bans,
                 'client' : tar ? sys.os(tar) : "Unknown",
                 'version' : tar ? sys.version(tar) : "Unknown",
-                'teams' : tar && (sys.auth(src) > 2 || isSuperAdmin(src)) ? teams : "Unknown"
+                'teams' : tar && (sys.auth(src) > 2 || isSuperAdmin(src)) ? teams : "Unknown",
+                'uniqueid' : tar && (sys.auth(src) > 2 || isSuperAdmin(src)) ? sys.uniqueId(tar).id : "Unknown"
             };
             sys.sendMessage(src, "+UserInfo: "+JSON.stringify(userJson), channel);
         } else if (command == "userinfo") {
