5d37d328827d1c287538c5186e870d9496202278
ET54321 <etthomas2010@yahoo.com>
2017-09-16 20:45:00 -0500

    Add more ways to end events

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -1313,11 +1313,13 @@ TriviaGame.prototype.finalizeAnswers = function () {
         }
     }
 
-    var neededWinners = 1;             //winners needed to end game
+    var neededWinners = 1;             //winners needed to end game, always 1 for non-event games
     if (trivData.eventFlag && this.scoreType !== "elimination"){  //for event know and event speed, extend the game
+        neededWinners = 4;
         if(totalPlayers > 3) { neededWinners = 3; }
-        if(totalPlayers === 3) { neededWinners = 2; }
-        //otherwise, it's just 1.
+        if(totalPlayers === 3 && this.round >= 10) { neededWinners = 2; } // include round to give players more time to win if player count drops
+        if(totalPlayers === 2 && this.round >= 10) { neededWinners = 1; }
+        if(leaderboard[0][1] >= (this.maxPoints*3)) { neededWinners = 1; } // ends games sooner when some people dominate and the rest struggle / afk
     }
      if (winners.length > (neededWinners - 1) || (this.scoreType === "elimination" && leaderboard.length === 0)) {
          /*if (trivData.eventFlag) {
