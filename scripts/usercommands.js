e1e1794088a0484e3ff71b47e7f40a2ca0503aaa
ET54321 <etthomas2010@yahoo.com>
2017-10-07 19:37:46 -0500

    Forgot to update the command list

--- scripts/usercommands.js
+++ scripts/usercommands.js
@@ -1113,7 +1113,7 @@ exports.help = [
     "/contributors: Lists contributors to Pokémon Online.",
     "/guide: Links to client guides and more. Inputing client type can list different guides.", // the code for this is in scripts.js
     "/intier [tier]: Displays all unidled players in a specific tier.",
-    "/league: Lists gym leaders and elite four of the PO league.",
+  //  "/league: Lists gym leaders and elite four of the PO league.",
     "/notice: Allows you to view current events.",
     "/players: Shows the number of players online. Can accept an operating system as argument to see how many users are using it.",
     "/topchannels: To view the most populated channels.",
