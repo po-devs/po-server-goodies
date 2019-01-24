2d15bfa81b05dac1ae46908970a30376d2182b13
Jordan Wandling <navi@Jordans-MacBook-Pro.local>
2019-01-24 11:26:34 -0800

    t

--- scripts/safari.js
+++ scripts/safari.js
@@ -2759,7 +2759,7 @@ function Safari() {
                 hasBalls = true;
             }
         }
-        var ph = "«" + link("/photo", "Take Photo") + "» ";
+        var ph = "«" + link("/photo", "Photo") + "» ";
         if (isAndroid && isBallAvailable(player, "master")) {
             sys.sendHtmlMessage(src, "<font color='#3DAA68'><timestamp/> <b>±Throw:</b></font> «" + link("/" + ccatch + " " + itemData.master.name, cap(itemData.master.name)) + "»", safchan);
         }
@@ -16524,9 +16524,8 @@ function Safari() {
         };
 
         var celebs = Object.keys(safari.celebrityTrainerData).shuffle().concat(Object.keys(safari.strongCelebrityTrainerData).shuffle());
-        var npc = {};
-        npc.name = celebs[0];
-        npc.party = JSON.parse(JSON.stringify(safari.celebrityTrainerData[npc.name])).shuffle().slice(0, 6);
+        var npc = JSON.parse(JSON.stringify(celebs.trainers[0]));
+        npc.party = npc.party.shuffle().slice(0, 6);
         npc.postBattle = postBattle;
         
         npc.postArgs = {
