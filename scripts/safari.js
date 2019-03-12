597b13f91320d2e3f5938a02a8fe0f087103203f
Miki Sayaka <45307509+MikiProductions@users.noreply.github.com>
2019-03-11 20:16:33 -0700

    t (#1843)
    * sd fix
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t

--- scripts/safari.js
+++ scripts/safari.js
@@ -23819,13 +23819,13 @@ function Safari() {
     };
     Volleyball.prototype.winGame = function(team) {
         if (team === 0) {
-            this.sendMessageAll("Team " + this.team1Data.name + " won the match! Congratulations to the winners!", "blue");
-            this.sendMessageAll(Object.keys(this.team1).join(",") + " are the champions!" , "blue");
+            this.sendMessageAll("Team " + this.teamData[0].name + " won the match! Congratulations to the winners!", "blue");
+            this.sendMessageAll(Object.keys(this.teams[0]).join(",") + " are the champions!" , "blue");
         }
         if (team === 1) {
             this.team2Data.score++;
-            this.sendMessageAll("Team " + this.team2Data.name + " won the match! Congratulations to the winners!", "blue");
-            this.sendMessageAll(Object.keys(this.team2).join(",") + " are the champions!" , "blue");
+            this.sendMessageAll("Team " + this.teamData[1].name + " won the match! Congratulations to the winners!", "blue");
+            this.sendMessageAll(Object.keys(this.teams[1]).join(",") + " are the champions!" , "blue");
         }
         this.finished = true;
         return;
@@ -23834,12 +23834,12 @@ function Safari() {
         var p, team2;
         this.points[team]++;
         if (team === 0) {
-            this.team1Data.score++;
-            this.sendMessageAll("Team " + this.team1Data.name + " scored a point!", "blue");
+            this.teamData[0].score++;
+            this.sendMessageAll("Team " + this.teamData[0].name + " scored a point!", "blue");
         }
         if (team === 1) {
-            this.team2Data.score++;
-            this.sendMessageAll("Team " + this.team2Data.name + " scored a point!", "blue");
+            this.teamData[1].score++;
+            this.sendMessageAll("Team " + this.teamData[1].name + " scored a point!", "blue");
         }
         if (this.points[team] >= 15) {
             this.winGame(team);
@@ -23848,7 +23848,7 @@ function Safari() {
         if (this.teamServed !== team) {
             this.sideOut(team);
         }
-        this.sendMessage("Score: " + this.team1Data.name + ": " + this.team1Data.score + " x " + this.team2Data.name + ": " + this.team2Data.score);
+        this.sendMessage("Score: " + this.teamData[0].name + ": " + this.teamData[0].score + " x " + this.teamData[1].name + ": " + this.teamData[1].score);
         team2 = (team === 0 ? 1 : 0);
         this.turn = 0;
         this.resetPosition(team2);
@@ -24561,17 +24561,18 @@ function Safari() {
         var team, player, opt = [], q, hold;
         var cdata = data.split(":");
 
-        if (Object.keys(this.team1).indexOf(name) !== -1) {
+        if (Object.keys(this.teams[0]).indexOf(name) !== -1) {
             team = 0;
             player = this.teams[0][name];
             teammates = this.teams[0];
         }
         else {
             team = 1;
-            player = this.team[1][name];
+            player = this.teams[1][name];
             teammates = this.teams[1];
         }
         if (!player) {
+            this.sendMessage(name, "Error handling command.");
             return;
         }
         
