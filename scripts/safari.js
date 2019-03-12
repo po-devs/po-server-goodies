6cb98269b92c2b674f5cdc03aa82c6277669cea7
Miki Sayaka <45307509+MikiProductions@users.noreply.github.com>
2019-03-11 17:28:56 -0700

    t (#1834)
    * sd fix
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t

--- scripts/safari.js
+++ scripts/safari.js
@@ -30842,7 +30842,7 @@ function Safari() {
         }
         if (currentGame && contestCooldown % currentGame.turnLength === 0) {
             currentGame.action();
-            if (currentEvent.finished) {
+            if (currentGame.finished) {
                 currentGame = null;
                 //checkUpdate(); //test
             }
