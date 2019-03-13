9e1f1668d3ccf987a27a8271ed4a4010e842dca6
Miki Sayaka <45307509+MikiProductions@users.noreply.github.com>
2019-03-13 14:46:27 -0700

    t (#1878)
    * sd fix
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t

--- scripts/safari.js
+++ scripts/safari.js
@@ -23388,6 +23388,22 @@ function Safari() {
             }
         }
     };
+    Volleyball.prototype.inputVal = function(name, k, v) {
+        var cteam, torow, column;
+        for (var team in this.teams) {
+            cteam = this.teams[team];
+            for (var t in cteam) {
+                p = cteam[t];
+                if (p.id !== name) {
+                    continue;
+                }
+                this.teams[team][t][k] = v;
+                return true;
+            }
+        }
+        this.sendMessageAll("Error inputting value for " + name + ".");
+        return false;
+    }
     Volleyball.prototype.movePlayer = function(name, goTo) {
         var cteam, torow, column;
         for (var team in this.teams) {
@@ -23492,7 +23508,7 @@ function Safari() {
                     continue;
                 }
                 if (p.action == "block" && p.zone == "front" && p.row == 4) {
-                    p.blocking = true;
+                    this.inputVal(player.id, "blocking", true);
                 }
             }
         }
@@ -23833,8 +23849,8 @@ function Safari() {
                 for (var s in p.actSkills) {
                     p.actSkills[s] = false;
                 }
-                p.action = "";
-                p.canServe = false;
+                this.inputVal(p.id, "action", "");
+                this.inputVal(p.id, "canServe", false);
             }
         }
         this.excludeActions = [];
@@ -24695,7 +24711,7 @@ function Safari() {
                     return false;
                 }
                 this.sendMessage(name, "Serve effort will be " + hold + "!", "red");
-                player.serveEffort = hold;
+                this.inputVal(player.id, "serveEffort", hold);
                 return true;
             }
         }
@@ -24741,7 +24757,8 @@ function Safari() {
                         if (this.excludeActions.indexOf("attack") !== -1) {
                             this.sendMessage(name, "A teammate is already making a play on the ball!", "red");
                         }
-                        player.action = "set";
+                        this.inputVal(player.id, "action", "set");
+                        this.inputVal(player.id, "setTarget", cdata[1]);
                         this.excludeActions.push("set");
                     }
                 }
@@ -24844,15 +24861,15 @@ function Safari() {
             this.sendMessage(name, "There is already a teammate going to " + data + "!", "red");
             return;
         }
-        if (player.action == "block") {
+        if (data == "block") {
             player.blocking = true;
-            player.action = data;
+            this.inputVal(player.id, "action", "block");
             this.sendMessageTeam(player.team, this.actName(player) + " is going to block!", "green");
         }
         else if ((data[0] === "x") || (data === "tip")) {
             this.excludeActions.push("tip");
             this.excludeActions.push("attack");
-            player.action = data;
+            this.inputVal(player.id, "action", data);
             if (data == "tip") {
                 this.sendMessageTeam(player.team, this.actName(player) + " is performing a tip!", "green");
             }
@@ -24867,7 +24884,7 @@ function Safari() {
             else {
                 this.excludePos[player.team].push(player.action);
                 this.sendMessageTeam(player.team, this.actName(player) + " is planning to move to " + data + "!", "green");
-                player.action = data;
+                this.inputVal(player.id, "action", data);
             }
         }
     };
