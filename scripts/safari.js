9beb31cc060564ebd00e1b171605ae2a70ca0c41
Jordan Wandling <navi@Jordans-MacBook-Pro.local>
2019-03-15 13:10:00 -0700

    t

--- scripts/safari.js
+++ scripts/safari.js
@@ -24638,7 +24638,7 @@ function Safari() {
             teammates = this.teams[1];
         }
         if (!player) {
-            this.sendMessageAll("Error handling command.");
+            this.sendMessageAll("Error handling command for " + name + ".");
             return;
         }
         if (cdata[0] == "eval") {
