8b0466ac6cd696554bb7c7b05ca4abf246e8cf1c
Jordan Wandling <navi@Jordans-MBP.hsd1.ca.comcast.net>
2019-12-03 05:40:37 -0800

    t

--- scripts/safari.js
+++ scripts/safari.js
@@ -43793,7 +43793,7 @@ function Safari() {
                     var themes = [];
                     for (var e in contestThemes) {
                         if (e !== "none" && safari.isInTheme(info.num, e)) {
-                            themes.push(themeName(contestThemes[e]));
+                            themes.push(themeName(e));
                         }
                     }
                     if (themes.length > 0) {
