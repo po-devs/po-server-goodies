faa7cf3245041afb74f728edb5a5c51a9b44b532
Jordan Wandling <navi@Jordans-MBP.hsd1.ca.comcast.net>
2019-12-05 16:21:15 -0800

    t

--- scripts/safari.js
+++ scripts/safari.js
@@ -2973,7 +2973,7 @@ function Safari() {
         },
         "716": {
             "name": "Branch Poke",
-            "learned": [709. 710, 810, 811, 812],
+            "learned": [708, 709, 810, 811, 812],
             "type": "Grass",
             "power": 40
         },
