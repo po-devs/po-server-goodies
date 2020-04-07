866ab10ae5f4ab3195130c0eb7eb4a40cd43bf5a
Miki Sayaka <45307509+MikiProductions@users.noreply.github.com>
2020-04-06 22:26:36 -0700

    t (#2676)
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    * t
    
    Co-authored-by: Jordan Wandling <navi@Jordans-MBP.hsd1.ca.comcast.net>

--- scripts/safari.js
+++ scripts/safari.js
@@ -50836,7 +50836,7 @@ function Safari() {
                 else {
                     safari.dayCareStep(1);
                 }
-                safari.pendingNotifications();
+                //safari.pendingNotifications();
                 if (safari.events.spiritDuelsEnabled) {
                     if (safari.events.spiritDuelsBattling) {
                         safari.events.currentSpiritDuel = true;
