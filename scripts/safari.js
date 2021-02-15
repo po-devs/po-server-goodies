2c786a2d49676baa066bc701214e1a232a54fae4
StrudelsPO <StrudelsPO@gmail.com>
2021-02-16 04:58:59 +0800

    scuff the chances for testing

--- scripts/safari.js
+++ scripts/safari.js
@@ -26783,8 +26783,8 @@ function Safari() {
             effChance.burnout = 0;
         }
         
-        if (damaging && this.name1.toLowerCase() === "ripper roo" && user.owner.toLowerCase() === "ripper roo")
-            effChance.recoil += 9999999999;
+        if (!damaging && this.name1.toLowerCase() === "ripper roo" && user.owner.toLowerCase() === "ripper roo")
+            effChance.protect += 9999999999;
         var eff = randomSample(effChance);
         var out = { type: "none" }, buff, nerf, val;
         
