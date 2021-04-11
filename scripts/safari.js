c6b4b5c2afde6b3dc20541589495e39ee476fb8b
StrudelsPO <StrudelsPO@gmail.com>
2021-04-11 19:15:09 +0800

    maybe a bit too glaring

--- scripts/safari.js
+++ scripts/safari.js
@@ -19799,7 +19799,7 @@ function Safari() {
             for (var a in team1) {
                 team1[a].rate = (team1[a].won / team1[a].fought);
                 if (team1[a].alive) {
-                    team1fighters += pokeInfo.icon(team1[a].mon) + " " + toColor(team1[a].owner.casedName + "'s " + poke(team1[a].mon), team1[a].owner.nameColor) + " " + toColor(" (" + this.toRate(team1[a].rate) + "%)  ", this.getSpiritDuelColor(team1[a].rate) );
+                    team1fighters += pokeInfo.icon(team1[a].mon) + " " + team1[a].owner.casedName + "'s " + poke(team1[a].mon) + " " + toColor(" (" + this.toRate(team1[a].rate) + "%)  ", this.getSpiritDuelColor(team1[a].rate) );
                 }
                 if (team1[a].rate < 0.5) {
                     team1[a].alive = false;
@@ -19814,7 +19814,7 @@ function Safari() {
             for (var a in team2) {
                 team2[a].rate = (team2[a].won / team2[a].fought);
                 if (team2[a].alive) {
-                    team2fighters += pokeInfo.icon(team2[a].mon) + " " + toColor(team2[a].owner.casedName + "'s " + poke(team2[a].mon), team2[a].owner.nameColor) + " " + toColor(" (" + this.toRate(team2[a].rate) + "%)  ", this.getSpiritDuelColor(team2[a].rate) );
+                    team2fighters += pokeInfo.icon(team2[a].mon) + " " + team2[a].owner.casedName + "'s " + poke(team2[a].mon) + " " + toColor(" (" + this.toRate(team2[a].rate) + "%)  ", this.getSpiritDuelColor(team2[a].rate) );
                 }
                 if (team2[a].rate < 0.5) {
                     team2[a].alive = false;
