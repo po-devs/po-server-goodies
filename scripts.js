f92edac4e7ef12ab6e34ef2da8b77e996a768359
StrudelsPO <StrudelsPO@gmail.com>
2021-08-28 17:41:20 +0800

    disable notice

--- scripts.js
+++ scripts.js
@@ -378,7 +378,7 @@ step: function() {
     }
 
     if (date.getUTCHours() % 3 === 0 && date.getUTCMinutes() === 0 && date.getUTCSeconds() === 0) {
-        sendNotice();
+        //sendNotice();
     }
     if (date.getUTCHours() % 1 === 0 && date.getUTCMinutes() === 0 && date.getUTCSeconds() === 0) {
         print("CURRENT SERVER TIME: " + date.toUTCString()); //helps when looking through logs
