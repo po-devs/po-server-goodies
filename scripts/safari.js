8ce8d89ee2053d914652babfdc146f7054ee37d4
StrudelsPO <StrudelsPO@gmail.com>
2022-04-01 03:46:28 +0800

    minor fix

--- scripts/safari.js
+++ scripts/safari.js
@@ -11274,7 +11274,7 @@ function Safari() {
                 }
             }
             if (player.options.sellPrompt && !isRare(currentPokemon)) {
-                safaribot.sendHtmlMessage(src, "«{0}» ({1})".format(link("/sell " + poke(currentPokemon, true) + ":confirm", "Click here to sell the " + poke(currentPokemon, true) + " you just caught"), "You now have " + countRepeated(player.pokemon, currentPokemon) + " " + poke(currentPokemon, true)), safchan);
+                safaribot.sendHtmlMessage(src, "«{0}» ({1})".format(link("/sell " + poke(currentPokemon) + ":confirm", "Click here to sell the " + poke(currentPokemon, true) + " you just caught"), "You now have " + countRepeated(player.pokemon, currentPokemon) + " " + poke(currentPokemon, true)), safchan);
             }
             if (contestCount > 0) {
                 var nameLower = name.toLowerCase();
