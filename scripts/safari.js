1788ddd60cfce409ae8ab47b33e48405bc1c6783
StrudelsPO <StrudelsPO@gmail.com>
2022-06-25 12:29:09 +0800

    fix origin portal accessing nonexistent variant

--- scripts/safari.js
+++ scripts/safari.js
@@ -8413,7 +8413,7 @@ function Safari() {
                             include = include.concat(theme.variations[variation]);
                         }
                     }
-                    else if (currentThemeEffect) {
+                    else if (currentThemeEffect && !currentThemeSecondary) {
                         include = include.concat(theme.variations[currentThemeEffect]);
                     }
                     if (currentThemeAlter && theme.alter) { // alter pool overrides everything
